fe7ebef3266e761b9b1f6c71c35b609987dc2837
mefrem <maxim.efremov@gmail.com>
2020-06-05 12:29:47 -0400

    Checkpoint ex2 with comments

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,19 +9,25 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # create a hashtable
     cache = {}
     
+    # for each ticket
+    # cache[source] = destination
     for ticket in tickets:
         cache[ticket.source] = ticket.destination
-    print(cache.items())
+    
+    # initialize empty route, first entry is our ticket with source NONE
     route = []
     route.append(cache["NONE"])
-    print(route)
-    searching = True
-    while searching is True:
-        for ticket in tickets:
-            _next = route[-1]
-            route.append(cache[_next])
-            if route[-1] == "NONE":
-                searching = False
-                return route
+
+    # Iterate through tickets, checking in constant time against cache
+    for ticket in tickets:
+
+        # Our next source is the last lcoation in our route
+        _next = route[-1]
+        # Grab that source's destination
+        route.append(cache[_next])
+        # if final location is NONE, we are done
+        if route[-1] == "NONE":
+            return route
