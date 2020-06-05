51577ad1cce21fb13f3c9c47c2a1382bc5f4bf50
James Luttringer <jamesluttringer18@gmail.com>
2020-06-05 10:13:28 -0400

    trip route

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,18 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    table = {}
+    route = []
+    for ticket in tickets:
+        if ticket.source == "NONE":
+            table['start'] = ticket.destination
+        else:
+            table[ticket.source] = ticket.destination
+    
+    key = 'start'
+    while table[key] != 'NONE':
+        route.append(table[key])
+        key = table[key]
+        
 
     return route
