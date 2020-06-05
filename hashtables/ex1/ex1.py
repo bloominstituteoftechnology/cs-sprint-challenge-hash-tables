07c377106e693ee5d4fb4c2c085bbf8bde7a42bd
Daanish Rasheed <daanishrasheed@gmail.com>
2020-06-05 10:40:39 -0400

    done with ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,12 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for i in range(len(weights)):
+        cache[weights[i]] = i
+    for i in range(len(weights)):
+        lw = limit-weights[i]
+        if lw in cache:
+            return (max(i, cache[lw]), min(i, cache[lw]))
 
     return None
