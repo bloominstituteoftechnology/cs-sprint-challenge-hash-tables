7d043edc745eda40b253af83115263ef88fa9a5b
qweliant <qweliantanner@gmail.com>
2020-06-08 08:42:02 -0400

    ex 1 & 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,33 @@
+#  Hint:  You may not need all of these.  Remove the unused functions.
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
+
+
 def get_indices_of_item_weights(weights, length, limit):
+    ht = HashTable(16)
+
     """
     YOUR CODE HERE
     """
-    # Your code here
+    for index, item in enumerate(weights):
+        hash_table_insert(ht, item, index)
+
+    for index, item in enumerate(weights):
+        goal = limit - item
+        lookup = hash_table_retrieve(ht, goal)
+        if lookup is not None:
+            return max(index, lookup), min(index, lookup)
 
     return None
+
+
+def print_answer(answer):
+    if answer is not None:
+        print(str(answer[0] + " " + answer[1]))
+    else:
+        print("None")
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,13 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
+
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +15,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    hashtable = HashTable(length)
+    route = [None] * (length - 1)
+
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ticket = None
+    for paper in tickets:
+        hash_table_insert(hashtable, paper.source, paper)
+        if paper.source == "NONE":
+            ticket = paper
 
+    counter = 0
+    while ticket.destination != "NONE":
+        route[counter] = ticket.destination
+        counter += 1
+        ticket = hash_table_retrieve(hashtable, ticket.destination)
+    j = route
+    j.append("NONE")
+    # print(j)
     return route
