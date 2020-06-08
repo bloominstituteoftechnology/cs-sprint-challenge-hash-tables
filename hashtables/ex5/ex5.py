1ee9a632d0d55ca164d91f43f82ed0c92da623ec
qweliant <qweliantanner@gmail.com>
2020-06-08 09:04:16 -0400

    done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,26 @@
-# Your code here
-
+import os
 
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    table = {}
+    for f in files:
+        base = os.path.split(f)[-1]
+        if base in table:
+            table[base].append(f)
+        else:
+            table[base] = [f]
+    result = []
+    for q in queries:
+        if q in table:
+            result += table[q]
 
     return result
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
+    files = ["/bin/foo", "/bin/bar", "/usr/bin/baz"]
+    queries = ["foo", "qux", "baz"]
     print(finder(files, queries))
