bf1154663136c7f43bd537815167f66368bb7aa4
Tristan Depew <tristan.depew@gmail.com>
2020-06-08 01:50:51 -0400

    tristan-depew

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,40 @@
+weights = [4, 6, 10, 15, 16]
+
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ht = {}
+
+    # for i, e in enumerate(weights):
+    #     cache[e] = i
+
+    # if cache[e] +
+    # return cache
+
+    for i in range(len(weights)):
+
+        # current weight in iteration is the weight at that index
+        curr_weight = weights[i]
+
+        # if the current weight is in the cache, that means the other matching weight index is the value in the cache
+        if curr_weight in ht:
+
+            # previous/matching weight index is the value
+            prev_weight = ht[curr_weight]
+            print(ht)
+
+            # i is the larger (current) weight, prev_weight is smaller
+            return (i, prev_weight)
+
+        print('current needed weights:', ht)
+
+        # hash table key is the other weight needed to reach limit
+        # value is the current weight's index
+        ht[limit - curr_weight] = i
 
     return None
+
+
+print(get_indices_of_item_weights(weights, 5, 21))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,10 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+
+
+# in hash, starting location is the key and destination is value
+# for ticket 1: {NONE: PDX}
+
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -9,6 +15,37 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    route = [None] * length
+    # initialize array of route with the length of the route
+
+    ht = {}
+    # make a hash table to store the trip sources and destinations
+
+    for t in tickets:
+        # for every Ticket in tickets, set the key as the source and the destination as the value
+        ht[t.source] = t.destination
+
+    # what is the destination which has "NONE" as the key, meaning the starting ticket?
+    next_destination = ht["NONE"]
+    # so now the next_destination is the VALUE in the hash table in which the KEY is "NONE"... this is the start destination, which is the first one to add to the array
+
+    # i is current ticket
+    for i in range(0, length):
+        # add value to array
+        route[i] = next_destination
+
+        next_destination = ht[next_destination]
+        # the next destination is the VALUE in the hash table in which the KEY is the destination just added to the array, which will be the next leg of the trip... keep doing this until at the end of the length of tickets(trip)
 
     return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+expected = ["PDX", "DCA", "NONE"]
+
+print(reconstruct_trip(tickets, 3))
