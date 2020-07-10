3f1739db383407a07f375372992d3bd707562707
Reece Pierson <piersonreece@gmail.com>
2020-07-10 11:01:32 -0500

    completes challenge 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,16 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    answer = None
+    for item in range(length):
+        lookFor = limit - weights[item]
+        if lookFor in cache :
+            if item <= lookFor:
+                answer = (item, cache[lookFor])
+            else:
+                answer = (cache[lookFor], item)
+        else:
+            cache[weights[item]] = item
+    return answer
 
-    return None
