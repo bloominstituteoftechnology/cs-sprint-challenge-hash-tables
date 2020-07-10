7042579363af9ffc6c7c6d71c44a7cd5314f6f03
Jake Krafczyk <jakekrafczyk@gmail.com>
2020-07-10 11:59:42 -0400

    sprint

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,39 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+    output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+    higher value's index goes first
     """
-    # Your code here
+    cache = {}
+
+    # for index, val in enumerate(weights):
+    #     if val in cache:
+    #         if cache[val] in cache:
+    #             print(cache[val])
+
+    #     else:
+    #         cache[val] = val - limit
+    #         if cache[val] in cache:
+    #             print(cache[val])
+    #             #return []
+
+    for index, val in enumerate(weights):
+        cache[limit-val] = index
+
+    print(cache)
+    for index, val in enumerate(weights):
+        if val in cache:
+            index2 = cache[val]
+            match = weights[index2]
+            if match >= val:
+                print(match)
+                return [index2,index]
+            else:
+                print(match)
+                return [index,index2]
+
 
     return None
+
+
+print(get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40],9,7))
