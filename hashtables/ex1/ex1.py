b42ff36bd1f35076321c482e9c99947b75e61749
Chase Goldfeld <goldfeld.chase@gmail.com>
2020-07-10 11:54:42 -0400

    Ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # $$$
+    cache = {}
 
+    # make the keys the weights and the values the indices
+    for i in range(len(weights)):
+        cache[weights[i]] = i
+    # print(cache)
+    # for each weight find the difference (complement) and if it matches,
+    # return the indices 
+    for i in range(len(weights)):
+        complement = limit-weights[i]
+        if complement in cache:
+            return (max(i, cache[complement]), min(i, cache[complement]))
+    
     return None
