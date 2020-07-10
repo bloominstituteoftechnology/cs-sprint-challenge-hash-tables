3e75a6da9907c07bc954ccf235d2ccb418b022bb
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-07-10 17:00:58 -0400

    got 3 out of 5

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,13 +1,16 @@
 def has_negatives(a):
     result = []
-    
+    my_dict = {}
+            
+    # loop though list
     for i in a:
-        if i <= 0:
-            a.remove(i)
-        else:
-            result.append(i)
+        my_dict[i] = i
+        # if num in not 0 or in dictionary
+        if i != 0 and -i in my_dict:
+            # return num as positive number
+            result.append(abs(i))
+                
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
