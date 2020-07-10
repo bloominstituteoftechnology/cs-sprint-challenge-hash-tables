9b5a8a180667d2ae9a0f5b4388bcc1d265e39920
Priya <pvaidya56@gmail.com>
2020-07-10 10:30:25 -0700

    ex5.py

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,21 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+
+    file_directory = dict()
+
+    for file in files:
+        file_split = file.split("/")
+        filename = file_split[-1]
+        if filename not in file_directory:
+            file_directory[filename] = []
+        file_directory[filename].append(file)
+
+    for query in queries:
+        if query in file_directory:
+            result.extend(file_directory[query])
+
 
     return result
 
