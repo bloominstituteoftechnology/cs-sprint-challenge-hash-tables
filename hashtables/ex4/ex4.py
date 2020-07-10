cbefd468ec6ec53144a879c7af69d2c616c38492
Jud Taylor <jud.taylor@protonmail.com>
2020-07-10 18:20:43 -0400

    works, tested

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,26 +1,22 @@
 def has_negatives(a):
-
-    list_of_integers = a
-
-    dict_of_integers = {}
-
-    for integer in list_of_integers:
-        if integer > 0:
-            dict_of_integers[integer] = 'positive'
-
-    for integer in list_of_integers:
-        if integer < 0 and -integer in dict_of_integers:
-            dict_of_integers[-integer] = 'both'
-
-    # print( dict_of_integers )
-
-    ret_list = [k for (k, v) in dict_of_integers.items() if v == 'both']
-
-    result = ret_list
-
-    # print(ret_list)
-
-    # return result
+    numbers = a
+    
+    positives = {}
+    non_positives = {}
+    retval = []
+    
+    for n in numbers:
+        if n > 0:
+            positives[n] = n
+            
+        else:
+            non_positives[n] = n
+            
+    for p in positives:
+        if -p in non_positives:
+            retval.append(p)
+            
+    return retval
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
