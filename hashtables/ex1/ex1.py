f6e89f43cfc4b8fcd6a8aa6ebec3bf911fcc9d24
Aaron Cleveland <toscleveland@gmail.com>
2020-07-10 11:09:28 -0500

    ex1 finished

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
 
+    if length < 2:
+            return None
+
+    for i in range(length):
+        cache[weights[i]] = i
+
+    for index, weight in enumerate(weights):
+        target_weight = limit - weight
+        
+        if target_weight in cache:
+            
+            target_index = cache[target_weight]
+            return (index, target_index) if index > target_index else (target_index, index)
     return None
