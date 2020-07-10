d3d769032b9dc1e5536a400c9e6c934b6940438f
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-07-10 12:26:21 -0400

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# Given a package with a weight limit `limit` and a list `weights` of item
+# weights, implement a function `get_indices_of_item_weights` that finds
+# two items whose sum of weights equals the weight limit `limit`.
 
+def get_indices_of_item_weights(weights, length, limit):
+    hashmap = {}
+    
+    # loop through list
+    for i in range(length):
+        # store each weight as key
+        weight = weights[i]
+        # if weight in hashmap
+        if weight in hashmap:
+            # store weight index as value
+            value = hashmap[weight]
+            # return tuple with (index, value)
+            return (i, value)
+        # calc difference
+        diff = limit - weight
+        # add to hashmap
+        hashmap[diff] = i
+        
     return None
