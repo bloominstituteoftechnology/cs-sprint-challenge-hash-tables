4b36876f69b814fb890c10969b140825114e6842
Jon Bash <jonbash@protonmail.com>
2020-07-10 08:17:39 -0700

    complete ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    lookup = {weight: i for (i, weight) in enumerate(weights)}
+    for (i, weight) in enumerate(weights):
+        needed_weight = limit - weight
+        if needed_weight in lookup:
+            return (max(i, lookup[needed_weight]), min(i, lookup[needed_weight]))
     return None
+
+if __name__ == '__main__':
+    output = get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21)
+    print(output)
