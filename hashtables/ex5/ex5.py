d5e71fdc161f91a1339e2efa6b13ecaf1db1b1d6
Luis <ocasio.dev@gmail.com>
2020-07-10 12:50:51 -0400

    examples 1,2,5 completed

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,27 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
 
+    cache = {}
+    for path in files:
+        file_item = path.split('/')[-1]
+
+        if file_item in cache:
+            cache[file_item].append(path)
+        else:
+            cache[file_item] = [path]
+
+    result = []
+    for query in queries:
+        if query in cache:
+            results = cache[query]
+            for path in results:
+                result.append(path)
     return result
 
 
