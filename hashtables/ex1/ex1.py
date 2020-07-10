03076ddd67fb3572f9fc57e63c6233efdd6b4b48
Pete1701 <pete.pertongonas@pirron.one>
2020-07-10 18:38:35 +0200

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,21 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    storage = dict()
+
+    for i in range(length):
+        # print(weights[i], 'test')
+        # weight = weights[i]
+        # print(weight)
+        if weights[i] in storage:
+            previous_weight_index = storage[weights[i]]
+            print(previous_weight_index)
+            return (i, previous_weight_index)
+
+        storage[limit - weights[i]] = i
+        # print(storage)
 
     return None
+
+# w = [5, 6, 7, 8, 9]
+# get_indices_of_item_weights(w, 5, 13)
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,18 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    common_int = dict()
+    result = []
+
+    for a in arrays:        
+        for num in a:
+            if num in common_int:
+                common_int[0] += 1
+            else:
+                common_int[0] = 1
+
+            for num in common_int:                
+                result = [num for num in common_int]
 
     return result
 
@@ -15,3 +27,9 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+    # for i in arrays:
+    #     a = arrays[i]
+    #     for value in a:
+    #         result = [value for value in a if value in a] 
+    # return result 
