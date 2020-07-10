bfef665426f1986efc604a31e17c7fad84291f2e
Jon Bash <jonbash@protonmail.com>
2020-07-10 08:59:54 -0700

    complete ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,22 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    paths_by_file = {}
+    for path in files:
+        components = path.split('/')
+        filename = components[-1]
+        if filename in paths_by_file:
+            paths_by_file[filename].append(path)
+        else:
+            paths_by_file[filename] = [path]
+    result = []
+    for query in queries:
+        if query in paths_by_file:
+            result.extend(paths_by_file[query])
     return result
 
 
