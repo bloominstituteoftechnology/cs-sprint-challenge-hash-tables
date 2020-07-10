0b29e067542b27d2e0bc79a865eeb70d262df6ac
Michael McGrath <maybemichael@icloud.com>
2020-07-10 11:02:33 -0700

    Hash Table CS Sprint

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,30 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    weight_dict = {}
+    one = None
+    two = None
+    for i in range(length):
+        if weights[i] not in weight_dict:
+        # weight_dict[weights[i]] = i
+            weight_dict.setdefault(weights[i], [i])
+        else:
+            weight_dict[weights[i]].append(i)
+    for i in range(length):
+        answer = limit - weights[i]
+        if answer in weight_dict:
+            if len(weight_dict[answer]) > 1:
+                one = weight_dict[answer][0]
+                two = weight_dict[answer][1]
+            else:
+                
+        # if answer in weight_dict:
+        #     if answer is weight_dict[answer]:
+        #         return weight_dict[answer][1], weight_dict[answer][0]
+        #     else:
+        #         return weight_dict[answer], i
+    
 
-    return None
+weights_2 = [4, 4]
+answer2 = get_indices_of_item_weights(weights_2, 2, 8)
+print(answer2)
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,10 +3,30 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    intersections = {}
+    list_dicts = []
+    result = {}
+    for i in range(len(arrays)):
+        num_dicts = intersections.fromkeys(arrays[i], i)
+        list_dicts.append(num_dicts)
+    print(num_dicts)
+    # for key, value in list_dicts[0].items():
+    #     for i in range(len(list_dicts)):
+    #         if key in list_dicts[i]:
+    #             result[i] = key
+    # for list in arrays:
+    #     list_count += 1
+    #     for num in list:
+    #         if list_count is 1 and num not in intersections:
+    #             intersections.setdefault(num, [list_count])
+    #         if num in intersections:
+    #             intersections[num].append(list_count)
+    # for key, value in intersections.items():
+    #     if value.count is len(arrays):
+    #         result.append(key)
 
     return result
-
-
+    
 if __name__ == "__main__":
     arrays = []
 
