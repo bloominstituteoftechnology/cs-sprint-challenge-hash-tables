d3193b352d9ce134e0300687f7fec43e362c93f9
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-07-10 13:48:10 -0400

    passing ex2_tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,25 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    route = []
+    mess_of_tickets = {}
+    # for each list of tickets, 
+    # iterate through the list 
+    for ticket in tickets:
+
+        # put each tuple into a key:value pair dictionary
+        mess_of_tickets[ticket.source] = ticket.destination
+
+    # starting with key of none
+    location = mess_of_tickets['NONE']
+    while location:  
+        # add the key of each pair to a list  
+        route.append(location)
+        # find correspond none's value key:value pair
+        location = mess_of_tickets[location]
+        if location == 'NONE':
+            route.append(location)
+            break
+
+    # return the list
     return route
