18775194d2079c44210e2a1db2293a41e0566d81
Doina Langille <doina.jitoreanu@gmail.com>
2020-07-10 11:56:33 -0400

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,52 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+# create cache
+cache = {}
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
-    """
-    # Your code here
+    Reconstructs a trip given a list of tickets.
+    Each tickect has a source and a destination.
+    The ticket for the first flight has source=None.
+    The ticket of the final flight has destination=None.
 
+    Returns:
+    -------
+    An array of strings with the route of the trip
+    """
+    # create an empty list in which we will save the airports
+    route = []
+
+    # hash each ticket such that source is the key and destination is value
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    
+    # The first flight is the one that has source=None
+    route.append(cache['NONE'])
+
+    # add in route each flight
+    for idx in range(length):
+        # take the flight from cache
+        if route[idx] in cache:
+            # is it the first flight? If so, move on
+            if cache[route[idx]] == route[0]:
+                continue
+            # add the next destination
+            route.append(cache[route[idx]])
     return route
+
+tickets = [
+    Ticket("PIT", "ORD" ),
+    Ticket("XNA", "CID" ),
+    Ticket("SFO", "BHM" ),
+    Ticket("FLG", "XNA" ),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO" ),
+    Ticket("CID", "SLC" ),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT" ),
+    Ticket("BHM", "FLG" )
+]
+
+reconstruct_trip(tickets, 10)
