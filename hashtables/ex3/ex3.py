030551075e5cf09a33840d3d3b4e0bd8f6f317e1
Golden Eagle <joowoon.kang@biola.edu>
2020-07-10 09:30:25 -0700

    finished

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,10 +2,21 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # store number of intersected for each number
+    intersected = {}
 
-    return result
+    for array in arrays:
 
+        for number in array:
+
+            if number in intersected:
+                intersected[number] += 1
+            else:
+                intersected[number] = 1
+    
+    # return only the items that have an occurence equal to the length of the arrays
+    return [key for key, value in intersected.items() if value == len(arrays)]
+   
 
 if __name__ == "__main__":
     arrays = []
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,9 +2,20 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    dictionary = {}
+    positive_numbers = []
 
-    return result
+    for number in a:
+
+        # add all numbers to the dictionary
+        dictionary[number] = 1
+
+        # if the current number's additive inverse in the dictionary, add the positive version to the resulting array
+        # no zero
+        if number is not 0 and number * -1 in dictionary:
+            positive_numbers.append(abs(number))
+
+    return positive_numbers
 
 
 if __name__ == "__main__":
