3c9593718e5b022ef3da88a530bf1cbf35884db1
Jake Krafczyk <jakekrafczyk@gmail.com>
2020-07-10 12:16:18 -0400

    sprint q2

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,34 @@
 def has_negatives(a):
     """
-    YOUR CODE HERE
+    For an input list of integers, we wish to know which positive numbers
+    have corresponding negative numbers in the list.
+
+    Example input:
+
+    ```python
+    [ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ]
+    ```
+
+    Input can be in any order.
+
+    Example return value:
+
+    [ 1, 3, 4 ]
     """
-    # Your code here
+    result = []
+    cache = {}
+    for x in a:
+        cache[x] = x
+
+    for x in a:
+        if -x in cache:
+            if abs(x) not in result and x > 0:
+                result.append(abs(x))
 
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    a = list(range(5000000))
+    a += [-1,-2,-3]
+    print(has_negatives(a))
