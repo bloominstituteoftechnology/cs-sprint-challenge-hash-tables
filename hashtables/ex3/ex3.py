a8be79fed524e1d9925621e00527bffc44c910d3
Daanish Rasheed <daanishrasheed@gmail.com>
2020-07-10 13:54:15 -0400

    done with all 5

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -10,8 +10,8 @@ def intersection(arrays):
                 o[j] += 1
             else:
                 o[j] = 1
+    #Add them up
     result = [data[0] for data in o.items() if data[1] == len(arrays)]
-
     return result
 
 
