8128e9b1b1643e9fb2d2d00d75733f1a44941aac
tmbern <tmbern42@gmail.com>
2020-07-10 09:57:59 -0600

    completed ex4, positive and negative

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,14 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # store all the values from the input to a hash table
+    cache = {k: 1 for k in a}
+
+    # create a list from each value in the hash table if the value has a corresponding negative
+    # value in the hashtable and the original value is a positive number
+    result = [k for k, v in cache.items() if (k*-1) in cache and k > 0]
+
+    
 
     return result
 
