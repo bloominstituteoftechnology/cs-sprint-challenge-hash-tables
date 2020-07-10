94b3b2af309191444ae290f63b58cf47b0669fe1
Hector <hectoremil21@hotmail.com>
2020-07-10 11:40:19 -0400

    Clean up

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,7 +2,6 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    tuple = ()
     # Your code here
     for i, weight in enumerate(weights):
         for l in range(i+1, length):
