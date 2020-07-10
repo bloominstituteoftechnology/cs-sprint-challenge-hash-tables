e889f8dccf33b693ebe0367d57d7ffcc2fe865a5
Shawn Gee <shawngee@gmail.com>
2020-07-10 12:08:05 -0400

    Completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,32 @@
+def value_in_all_dicts(value, dicts):
+        for dict in dicts:
+            if value not in dict:
+                return False
+        
+        return True
+
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Go through each array and make a dict of unique values
+    # Add each dict to an array
+    # Take last dict, and for each value, check if all other dicts contain that value
+    # If so, add that value to result
+
+    unique_value_dicts = []
+
+    for array in arrays:
+        unique_values = {}
+        
+        for value in array:
+            unique_values[value] = True
+        
+        unique_value_dicts.append(unique_values)
+    
+    last_dict = unique_value_dicts[-1]
+    result = []
+
+    for value in last_dict:
+        if value_in_all_dicts(value, unique_value_dicts[:-1]):
+            result.append(value)
 
     return result
 
