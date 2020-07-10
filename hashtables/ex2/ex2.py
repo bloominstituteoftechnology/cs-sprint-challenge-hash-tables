0be84e9d1b3006c8b39d20d7fc2039dc6aecfa27
Reece Pierson <piersonreece@gmail.com>
2020-07-10 11:45:29 -0500

    finishes challenge 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,19 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    #iterate through list
+    #assign key of souce  and value of destination
+    #get key = "NONE" 
+        ##PRINT source or destination???
+    #continue until "NONE"
+    hashedTickets = {}
+    route = [None] * length
+    for i in range(length):
+        hashedTickets[tickets[i].source] = tickets[i].destination
+    location = hashedTickets['NONE']
+    for i in range(length):
+        route[i] = location
+        location = hashedTickets[location]
 
+    print('ROUTE: ', route)
     return route
