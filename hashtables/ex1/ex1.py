145d04e56bc50687952a80e57f09b700adbfb370
tmbern <tmbern42@gmail.com>
2020-07-10 09:46:30 -0600

    completed ex1, weight limits

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    
+    # loop thru each element in the weights. Use enumerate so we get an index and value for each weight
+    for i, num in enumerate(weights):
 
-    return None
+        # get the difference between the target limit and the current weight
+        n = limit - num
+
+        # if the difference isnt in the cache then store the current weight as a key and the index as value
+        if n not in cache:
+            cache[num] = i
+        
+        # if the difference is in the cache, this means that there is a key in the cache (an actual weight)
+        # that is the difference between the target and the current weight. So we want to return those
+        # two indices. 
+        else:
+            return [max(cache[n], i), min(cache[n], i)]
+
+    return None 
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,13 +4,23 @@ def intersection(arrays):
     """
     cache = {}
 
+    # loop thru each array
     for i in arrays:
+
+        # for each array loop thru each of the individual components of that array
         for j in i:
+
+            # if the element of the array is in the cache then increase the value of that key by one
             if j in cache:
                 cache[j] += 1
+            
+            # if the element of the array is not in the cache then save the element as a key and set value to 1
             else:
                 cache[j] = 1
 
+    # our result is returning each key in the cache if the value of the cache is equal to the length of our list of arrays
+    # i.e. if our list of arrays was 3 arrays long, then any key that had a value of 3 means that that key appeared 
+    # in all three arrays. 
     result = [k for k, v in cache.items() if v == len(arrays)]
 
     return result
