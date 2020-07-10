04b5745f69583c6fe34c55b30fb0c3dbcd4ca900
Shawn Gee <shawngee@gmail.com>
2020-07-10 11:52:32 -0400

    Completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    tickets_by_source = {}
+
+    for ticket in tickets:
+        tickets_by_source[ticket.source] = ticket
+
+    # first ticket would be source of "NONE"
+    # use it's destination to get next
+
+    current_ticket = tickets_by_source["NONE"]
+    
+    route = []
+
+    while True:
+        route.append(current_ticket.destination)
+    
+        if current_ticket.destination == "NONE":
+            break
+
+        current_ticket = tickets_by_source[current_ticket.destination]
 
     return route
