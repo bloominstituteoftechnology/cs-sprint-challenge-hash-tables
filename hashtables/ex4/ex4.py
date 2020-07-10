a3be3dd29f1e12516464f090915a557d40b87bd9
Hector <hectoremil21@hotmail.com>
2020-07-10 12:29:39 -0400

    Implement ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,15 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    dict = {}
+
+    for num in a:
+        final = num if num > 0 else num * -1
+        if final in dict:
+            result.append(final)
+        else:
+            dict[final] = 1
 
     return result
 
