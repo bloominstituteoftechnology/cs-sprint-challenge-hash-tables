4cd3f27acdb54d6a13a0dba213cb31916d8ede34
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-07-18 12:33:29 -0400

    Completed Example 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,15 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+	"""
+	YOUR CODE HERE
+	"""
+	# Your code here
+	for i in range(0, length):
+		for weight in weights:
+			if weights.index(weight) != i:
+				if weight + weights[i] == limit:
+					if weights.index(weight) > i:
+						return [weights.index(weight), i]
+					else:
+						return [i, weights.index(weight)]
 
-    return None
+	return None
