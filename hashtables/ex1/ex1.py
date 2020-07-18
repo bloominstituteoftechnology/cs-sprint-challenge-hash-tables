f734f5424fc84d127992d03c385bc575a2f7385c
DanSample <40527003+DanSample@users.noreply.github.com>
2020-07-17 18:45:19 -0700

    ex1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,17 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+  #initialize dictionary to store values
+  store = {}
+  #setting hash table (weights = keys. values = i)
+  #using enumerate to keep track of iterations w key value pairs
+  for i, weight in enumerate(weights):
+        store[weight] = i
+  #iterating through table checking weights that meet limit
+  for i, weight in enumerate(weights):
+        if limit - weight in store:
+              j = store[limit - weight]
+              if i > j:
+                  return (i, j)
+              else:
+                    return (j, i)
 
-    return None
+  return None
