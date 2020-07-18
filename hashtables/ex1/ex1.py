66b8c0376bce357bffc21ef3b2af4e05fa9957cd
kly001 <karenly1961@gmail.com>
2020-07-17 23:55:33 -0400

    Still working to get it right

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,16 +6,20 @@ def get_indices_of_item_weights(weights, length, limit):
 
     results = {}
 
-    for i in range(length):
-        weight= weights[i]
-        pair = limit - weight
-        if pair in results:
-            return[results[pair], i]
-        results[weight] = 1
+    # for i in range(length):
+    #     weight= weights[i]
+    #     pair = limit - weight
+    #     if pair in results:
+    #         return[results[pair], i]
+    #     results[weight] = 1
+    # return None
+
+    for i, weight in enumerate(weights):
+        if limit - weight in results:
+          return [results[limit-weight], i]
+        results[weight] = i
     return None
 
-weights = [1,4,8,3,2,9,12]
-limit = 13
-print(weights)
-print(limit)
-    
+
+
+print(get_indices_of_item_weights([1,4,8,3,2,9,12,5,6,33], 10, 13))
