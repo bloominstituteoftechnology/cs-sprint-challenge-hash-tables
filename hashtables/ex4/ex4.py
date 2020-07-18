bfbc5ca20657e51d51b0e8c8257bd2c23a17305a
kly001 <karenly1961@gmail.com>
2020-07-17 21:59:06 -0400

    ex4.py: needs edits to pass  tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    storage = {}
+    result = []
+
+    for number in a:
+        storage[number] = 1  # adds numbers to storage
+        if (-1*number) in storage: # looks for opposite of the numbers in storage
+            result.append(abs(number)) # appends the absolute value of the number into storage
+
 
     return result
 
