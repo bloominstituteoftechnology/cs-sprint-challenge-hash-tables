8cb9dd3ccc42348cf24ec298f34cbb9793dab7f4
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-07-18 13:08:54 -0400

    Added pseudo code and Example 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,27 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+	def __init__(self, source, destination):
+		self.source = source
+		self.destination = destination
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+	"""
+	YOUR CODE HERE
+	# Set all of the tickets into our cache
+	# Start our route where source equals NONE
+	# Reconstruct our route
+	"""
+	# Your code here
+	cache = {}
+	for ticket in tickets:
+		cache[ticket.source] = ticket.destination
 
-    return route
+	route = []
+	next_tic = cache["NONE"]
+	route.append(next_tic)
+	while next_tic != "NONE":
+	   next_tic = cache[next_tic]
+	   route.append(next_tic)
+
+	return route
