aef0deaaae5eee13d0059f0cccce2c78b9a878dd
ash12hub <ash12hub@users.noreply.github.com>
2020-07-18 08:02:42 +0530

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    limits = {}
+    start_index = None
+    pair = None
+    for i in range(len(weights)):
+        # Handle collisions
+        if weights[i] in limits:
+            limits[weights[i]].append(i)
+        else:
+            limits[weights[i]] = [i]
+        # If the pair is reached return the pair index and the first index
+        if weights[i] == pair:
+            print('end', i, limit-pair)
+            return(i, limits[limit-pair][0])
+        # Find a pair and save its value
+        if limit - weights[i] in weights:
+            pair = limit - weights[i]
+            print('start', i, pair)
+
     return None
