1c9261189289b05d9e09a87cfcab68b96aedb6e0
ash12hub <ash12hub@users.noreply.github.com>
2020-07-18 09:01:21 +0530

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,33 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+
+    route = [None] * length
+    current_target = 'NONE'
+    current_index = 0
+
+    for i in range(length):
+        # Add the source and the destination to the hash
+        hash_table[tickets[i].source] = tickets[i].destination
+        
+        # Store the next destination in the route and change the current target
+        if tickets[i].source == current_target:
+            current_target = tickets[i].destination
+            route[current_index] = current_target
+            # Exit if final destination is reached
+            if current_target == 'NONE':
+                break
+            current_index += 1
+
+            # Search current entries for the next destination
+            while current_target in hash_table:
+                current_target = hash_table[current_target]
+                route[current_index] = current_target
+                # Exit if final destination is reached
+                if current_target == 'NONE':
+                    break
+                current_index += 1
+
 
     return route
