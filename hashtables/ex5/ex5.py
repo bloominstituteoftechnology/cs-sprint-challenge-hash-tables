3ec0434c4191c21686bc44ef6fb74b6ae5205161
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-07-19 19:17:48 -0700

    Exercise 5.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,27 @@
-# Your code here
-
-
-
 def finder(files, queries):
     """
-    YOUR CODE HERE
+    Given a list of full paths to files, and a list of filenames to query,
+    report all the full paths that match that filename.
     """
-    # Your code here
 
+    # Build a lookup table using a dictionary as follows:
+    # Key: filename
+    # Value: [path_1, path_2, etc.]
+    lookup = {}
+    for path in files:
+        filename = path.split('/')[-1]
+        if filename not in lookup:
+            lookup[filename] = [path]
+        else:
+            lookup[filename].append(path)
+
+    # Build a list of full paths matching each of a list of filenames.
+    result = []
+    for query in queries:
+        if query in lookup:
+            result += lookup[query]
+
+    # Return all paths found.
     return result
 
 
