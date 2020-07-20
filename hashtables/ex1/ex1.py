ae5c71dd1bf1fa4a838e20cb62220cd1882565dc
Dave Kane <davelkane@gmail.com>
2020-07-19 20:44:53 -0500

    ex1.py complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,13 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Create a cache
+    cache = {}
+    # Traverse the weights list in reverse, store the weights as weight: index key:value pair
+    for i in reversed(range(len(weights))):
+        # Check to see if the the limit - the current value is in the cache
+        if limit - weights[i] in cache:
+            return [cache[limit - weights[i]], i]
+        if weights[i] not in cache:
+            # If the the current weight and index are not in the cache add it
+            cache[weights[i]] = i
 
     return None
