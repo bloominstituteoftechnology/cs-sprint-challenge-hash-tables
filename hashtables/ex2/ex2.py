ade294256377fcbf16cc32cda934e5c118d01940
Dave Kane <davelkane@gmail.com>
2020-07-19 22:16:22 -0500

    ex2.py complete, stretch complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,25 @@ class Ticket:
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# Create a cache
+cache = {}
 
+
+def reconstruct_trip(tickets, length):
+    # Traverse the ticket list and load the cache
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    # Create a results arr, preload the destination with no source
+    route = [cache["NONE"]]
+
+    # Traverse the cache by key, swapping the current dest for the new source
+    # appendending the airport identifiers until dest is None
+    curr = route[0]
+
+    while cache[curr] != "NONE":
+        dest = cache[curr]
+        route.append(dest)
+        curr = cache[curr]
+
+    route.append("NONE")
     return route
