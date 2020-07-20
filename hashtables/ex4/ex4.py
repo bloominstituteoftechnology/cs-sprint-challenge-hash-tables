361c4e7400decb136663275ef6a72be7c2387833
Dave Kane <davelkane@gmail.com>
2020-07-19 21:48:07 -0500

    ex4.py complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,20 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Create a cache
+    cache = {}
+    # Create a results arr
+    res = []
 
-    return result
+    # Traverse the input arr
+    for val in a:
+        # Check if the additive inverse val is in the cache
+        if -val in cache:
+            # If true, add the absolute value to the res arr
+            res.append(abs(val))
+        # If it's not in the cache, add it
+        else:
+            cache[val] = True
+
+    return res
 
 
 if __name__ == "__main__":
