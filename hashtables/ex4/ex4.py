cee07ed91409de20413aa8a74bc70340923fed44
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-08-07 09:50:47 -0700

    ex4 passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,14 +1,21 @@
 def has_negatives(arr):
-
+    # build hashtable and result arr
     ht = {}
     result = []
 
+    # loop over arr
     for num in arr:
+
+        # establish a negative val for each number we're looking at
         negative = num * -1
 
+        # check to see if the negative val is already in the hashtable
         if negative in ht:
+
+            # if so, push the absolute value onto the result arr
             result.append(max(num, negative))
 
+        # otherwise add the num and negative as a key/value pair
         else:
             ht[num] = negative
 
