d2d4cc2fd36188cebeea6b359da4a70256e6bd4e
Kelli Scheuble <github@scheuble.dev>
2020-08-07 08:53:28 -0700

    adds ex4 solution

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,13 +1,26 @@
 def has_negatives(a):
+    """
+    Params:
+    -----
+    a: list of both positive and negative integers
+
+    Returns:
+    -----
+    number if the negative value is also seen in the
+    list
+    """
 
     number_dict = dict()
     has_neg = list()
 
+    # iterate over list and check to see if that value * -1
+    # is seen in the dictinary.. if so, add it to the list. 
     for number in a:
         if (number * -1) in number_dict:
             has_neg.append(abs(number))
         number_dict[number] = True
     
+    # reurn a list with zero repeats
     return list(set(has_neg))
 
 
