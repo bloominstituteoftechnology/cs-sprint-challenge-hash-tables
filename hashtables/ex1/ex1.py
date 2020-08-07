056e825de5b16e6a05f55c7986d71958e82af1fa
Enrique Gongora <EGTech24@gmail.com>
2020-08-07 09:27:07 -0700

    Example 1 & 2 Done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,20 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    # We'll use the zip function to aggregate the iterables in a tuple
+    weightIndex = { i:j for i, j in zip(weights, range(0, len(weights)))}
+
+    # We'll use the range function, using 0 as the starting value.
+    for index in range(0, len(weights)):
+        # We'll get the current weight
+        currentWeight = weights[index]
+        # Then we'll calculate the target weight by subtracting the the currentweight from the limit
+        targetWeight = limit - currentWeight
+        # After our calculations, if the target weight is in our weight index
+        if targetWeight in weightIndex:
+            # if the current weight is greater than the target weight
+            if index > weightIndex[targetWeight]:
+                return (index, weightIndex[targetWeight])
+            else:
+                return (weightIndex[targetWeight], index)
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,29 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # Create an empty dict
+    cache = {}
 
+    # We'll set the route variable to an array with a null value, multiplied by the length of the list
+    route = [None] * length
+
+    # We'll loop through our tickets
+    for ticket in tickets:
+        # We'll use this to get our destinations into our list
+        cache[ticket.source] = ticket.destination
+    next = cache["NONE"]
+
+    # We'll loop through the length of the list
+    for current in range(0, length):
+        # Grab the current index in our route
+        route[current] = next
+        # Assign it our next variable
+        next = cache[next]
+    # After our list has been filled with the proper routes, we'll return it
     return route
+
+'''
+"None" - "PDX"
+"PDX" - "DCA"
+"DCA" - "NONE"
+'''
