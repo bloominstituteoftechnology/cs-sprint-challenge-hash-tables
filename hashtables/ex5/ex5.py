c3da799723a5fec0402812ca6e59f65b99b8c20f
Kelli Scheuble <github@scheuble.dev>
2020-08-07 08:56:42 -0700

    adds ex5 solution

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,7 +1,25 @@
 def finder(files, queries):
+    """
+    Params:
+    -----
+    files: list of file paths
+    queries: list of files to search for
+
+    Returns:
+    -----
+    results: list of file paths that contain the files in the
+    queries
+
+    """
 
     file_dict = dict()
 
+    # I want to create a dictionary with the file name
+    # being the key and the path being the value
+    
+    # get the name of the file by doing a right partition
+    # on the file path then adding that file name to the 
+    # dictionary
     for path in files:
         name = path.rpartition('/')[-1]
         if name not in file_dict:
@@ -11,6 +29,8 @@ def finder(files, queries):
 
     results = list()
 
+    # look over the queries to see if any of the paths
+    # lead to the files
     for query in queries:
         if query in file_dict:
             for path in file_dict[query]:
