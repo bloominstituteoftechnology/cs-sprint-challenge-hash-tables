1c4e098a16db44677371143572ed2695ab41b71e
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-08-07 12:11:28 -0400

    ex2 completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,21 @@ class Ticket:
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+def reconstruct_trip(tixlist, length):
+    for tick in tixlist:
+        flights[tick.source] = tick.destination
+    route = []
+    val = flights['NONE']
+    while val != 'NONE':
+        route.append(val)
+        val = flights[val]
+    route.append(val)
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets, len(tickets))
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,12 +1,29 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # x = range(len(arrays))
+    dict_list = []
+    for s in arrays:
+        dicto = {}
+        for i in s:
+            dicto[i] = True
+        dict_list.append(dicto)
+    cands = list(dict_list[0].keys())
+    # for s in range(1,len(arrays)):
+    #     for i in cands:
+    #         if dict_list[s][i]:
+    #             continue
+    #         cands.pop(i)
+    
+    return dict_list[1][2] #result
 
-    return result
+arroz = [
+    [1,2,3],
+    [1,4,5,3],
+    [1,6,7,3]
+]
 
+print(intersection(arroz))
 
+'''
 if __name__ == "__main__":
     arrays = []
 
@@ -15,3 +32,16 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+'''
+'''
+def intersection(arrays):
+    x = range(len(arrays))
+    cands = {}
+    for i in arrays[0]:
+        cands[i] = True
+    for s in arrays[1:]:
+        for i in s:
+            if not cands[i]:
+
+    return cands #result
+    '''
