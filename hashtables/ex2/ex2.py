4226652239b050e1c641dbee1e2f9188db22ecbe
Tim Shaker <tim@tmshkr.com>
2020-08-07 08:37:23 -0700

    complete ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,3 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +5,32 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    ht = {}
+    route = []
+
+    for t in tickets:
+        ht[t.source] = t.destination
+    
+    curr = ht["NONE"]
+    while curr != "NONE":
+        route.append(curr)
+        curr = ht[curr]
+    route.append("NONE") # append last "NONE" for tests
 
     return route
+
+
+tickets = [
+    Ticket("PIT", "ORD" ),
+    Ticket("XNA", "CID" ),
+    Ticket("SFO", "BHM" ),
+    Ticket("FLG", "XNA" ),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO" ),
+    Ticket("CID", "SLC" ),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT" ),
+    Ticket("BHM", "FLG" )
+]
+
+reconstruct_trip(tickets, len(tickets))
