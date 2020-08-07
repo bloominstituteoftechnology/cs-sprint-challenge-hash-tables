96947cbc98d47f00f6ef2b335478e5a61f79f772
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-08-07 09:41:10 -0700

    ex4 passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,19 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+def has_negatives(arr):
+
+    ht = {}
+    result = []
+
+    for num in arr:
+        negative = num * -1
+
+        if negative in ht:
+            result.append(max(num, negative))
+
+        else:
+            ht[num] = negative
 
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    print(has_negatives([-1, 1, -1, -4, -2, 1, 2, 3, 4, -4, -4, -7, 7]))
