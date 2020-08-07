26412331f7d698440339ccbf5e5c56589f878b6f
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-08-07 08:48:12 -0700

    Completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,4 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
+    indices = {}
+
+    for i in range(length):
+        indices[weights[i]] = i
+        target = limit - weights[i]
+        print(f"target: {target} = limit: {limit} - weights[i]: {weights[i]}")
+        if target in indices:
+            if indices[target] > i:
+                return (indices[target], i)
+            else:
+                return (i, indices[target])
+
     """
     YOUR CODE HERE
     """
