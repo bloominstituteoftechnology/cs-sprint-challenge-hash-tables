e90cb2389b1c034db001b14c028f00e3171bbf2c
wesley.moody <wgm3190@gmail.com>
2020-08-07 10:52:29 -0500

    ex2 tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,27 @@ class Ticket:
         self.destination = destination
 
 
+
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+
+    # Create a dict
+    store = {}
+
+    # Create a list the len of the length
+    route = [None] * length
+
+    # Iterate over list of tickets
+    for ticket in tickets:
+        #set the source in store to the destination
+        store[ticket.source] = ticket.destination
+    # reset the destination
+    destination = store['NONE']
+    # iterate over the length
+    for flight in range(length):
+        # set the route flight to destination
+        route[flight] = destination
+        # set the destination to the store
+        destination = store[destination]
 
     return route
