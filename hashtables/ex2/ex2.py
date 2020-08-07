8d382fbe5080c0cadbb8243eedf3fee943f3ac9d
Chance Rutledge <chancerutledge2@gmail.com>
2020-08-07 10:24:57 -0700

    ex2 added

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,25 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
+    def __init__(self, origin, destination):
+        self.origin = origin
         self.destination = destination
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dict = {}
+    route = []
+
+    for ticket in tickets:
+        # set the dictionary up with the origin as the key and destination as the value
+        dict[ticket.origin] = ticket.destination
+    # we know our first ticket has an origin of "NONE" so lets set our pointer
+    pointer = dict["NONE"]
+    # length is the number of tickets
+    # repeat this loop until all tickets have been arranged
+    while len(route) < length:
+        # append the pointer to the array
+        route.append(pointer)
+        # change the pointer to the value of the destination - not the origin
+        pointer = dict[pointer]
 
     return route
