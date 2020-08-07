a9aac1234e3356d81d52a98456e8ad11bbb2b72d
Tim Shaker <tim@tmshkr.com>
2020-08-07 08:24:49 -0700

    complete ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    ht = {}
 
+    for i, w in enumerate(weights):
+        target = limit - w
+    
+        if target in ht:
+            if ht[target] > i:
+                return (ht[target], i)
+            else:
+                return (i, ht[target])
+        else:
+            ht[w] = i
+    
     return None
+
+
+
+get_indices_of_item_weights([ 4, 6, 10, 15, 16 ],5,21)
