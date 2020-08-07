153d93d207349b75d7bc400a976c001a4198f57f
john <jgreenberg@runbox.com>
2020-08-07 13:53:32 -0400

    sprint challenge

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,13 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    counter = {}
 
-    return result
+    for array in arrays:
+        for small_array in array:
+            if small_array in counter:
+                counter[small_array] += 1
+            else:
+                counter[small_array] = 1
+    return [inter for inter in counter.keys() if counter[inter] == len(arrays)]
 
 
 if __name__ == "__main__":
