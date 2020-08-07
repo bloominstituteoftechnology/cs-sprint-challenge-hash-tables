3bc540acaf713ef1c214ef67aea85be8279aee43
Chance Rutledge <chancerutledge2@gmail.com>
2020-08-07 08:51:00 -0700

    ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,21 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dict = {}
+    result = []
+
+    # set value_count as the key (i.e. the value we are looking for) and track the number of times it appears
+    # confusing i know - but it makes sense in my head
+    for keys in arrays:
+        for value_count in keys:
+            if value_count in dict:
+                # track the number of items our value has appeared
+                dict[value_count] += 1
+            else:
+                # if the value the value is not in the dict we count it as a first occurance
+                dict[value_count] = 1
+            # if our value matches the number of arrays
+            # we know it appears in each array and intersects
+            if dict[value_count] == len(arrays):
+                result.append(value_count)
 
     return result
 
