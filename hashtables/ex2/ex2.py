c4cbddab53f3e1c32167e0ccddac02ab6d3b38ba
Kelli Scheuble <github@scheuble.dev>
2020-08-07 08:46:17 -0700

    adds ex2 solution

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,19 +6,39 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    """
+    Params:
+    -----
+    tickets = list of jumbled Ticket classes (starting flight has source of "NONE" and 
+    ending flight has destinatiton of "NONE")
+    length = length of ticket lists
+
+    Returns:
+    -----
+    list of organized flights
+
+    """
 
     ticket_dict = dict()
     organized_flights = list()
 
+    # iterate over all of the tickets and add to the dictionary
+    # with the souce being the key and the destination being the value
     for ticket in tickets:
         ticket_dict[ticket.source] = ticket.destination
 
+    # start with the value of the dict at source == NONE
+    # for first flight
     destination = ticket_dict["NONE"]
 
+    # go until destination equals NONE
     while destination != "NONE":
+        # add the destination to the organized flights
+        # then reassign destination to be the next value
         organized_flights.append(destination)
         destination = ticket_dict[destination]
 
+    # tests require that NONE be added at the end
     organized_flights.append(destination)
 
     return organized_flights
