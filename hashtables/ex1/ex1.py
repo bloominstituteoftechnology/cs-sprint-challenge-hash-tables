83b1bd3f7043ae5b1fa48bbb9bd914f6dfe75569
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-08-09 16:59:46 -0700

    Update ex1.py
    incomplete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,7 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    cache = {}
+    for i in range(length):
+        cache[weights[i]] = i
+   
+    
     return None
