ba1c667d4fe7f2e46cb493f03b7383f4d0e0bb7b
stevekutz <stkutz@gmail.com>
2020-08-15 11:08:47 -0500

    init

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,3 +5,12 @@ def get_indices_of_item_weights(weights, length, limit):
     # Your code here
 
     return None
+
+
+
+
+
+
+
+weights = [ 4, 6, 10, 15, 16 ]  # length = 5, limit = 21
+print(get_indices_of_item_weights(weights, len(weights), 21))
