d7ba5752bc5cff4d4bcfa0d475b6e8a17d5c9274
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-08-15 12:34:30 -0700

    Update ex1.py
    tests pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,14 @@
 def get_indices_of_item_weights(weights, length, limit):
+    # set cache
     cache = {}
+
+    # checks list of weights by index
     for i in range(length):
         cache[weights[i]] = i
-   
-    
+   # finds matching weights
+    for index, weight in enumerate(weights):
+        w = limit - weight
+        if w in cache:
+            i = cache[w]
+            return (index, i ) if index > i else (i, index)
     return None
