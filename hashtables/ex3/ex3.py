a39650a764e57f79724e3ff0081326855cedb456
Sheila R Moore <sherexmykes@github.com>
2020-08-14 21:41:06 -0400

    ex3 and ex4

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,17 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    if len(arrays) == 0:
+        return []
+
+    for nums in arrays:
+        for num in nums:
+            if num in cache:
+                cache[num] += 1
+            else:
+                cache[num] = 1
+    return [num for (num, count) in cache.items() if count == len(arrays)]
 
     return result
 
