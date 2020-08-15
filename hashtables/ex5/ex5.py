5de136dd5caa7be997f80c0af4c6d8a0a10f4ade
stevekutz <stkutz@gmail.com>
2020-08-15 13:39:47 -0500

    ex_5 completed, STRETCH #2 done, submitting Sprint retro

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -11,37 +11,44 @@ def finder(files, queries):
     query_cache = {}
     result = []
 
-
     for file in files:
         file_split = file.split('/')
         file_name = file_split[-1]
-        print(f' file_name: {file_name} in file: {file}')
-        if file not in file_cache:
-            file_cache[file] = file_name
+        # print(f' file_name: {file_name} in file: {file}')
+        if file_name not in file_cache:
+            file_cache[file_name] = []   # build list at key
+        file_cache[file_name].append(file)    
 
-    print(file_cache)
+    # print(file_cache)
 
     for query in queries:
         if query not in query_cache:
             query_cache[query] = query
 
-    print(f' \t query_cache {query_cache} ')
+    # print(f' \t query_cache {query_cache} ')
+
+    for query in query_cache:
+        if query in file_cache:
+            # print(f' CACHE HIT on query {query} ')
+            # result.append(file_cache[query]) # NOPE add list inside of list (e.g. obj <-> list gets appended)
+            # print(f' result {result}')
+            result.extend(file_cache[query])   # adds directly to list as args in list iterated 
 
     return result
 
 
-# if __name__ == "__main__":
-#     files = [
-#         '/bin/foo',
-#         '/bin/bar',
-#         '/usr/bin/baz'
-#     ]
-#     queries = [
-#         "foo",
-#         "qux",
-#         "baz"
-#     ]
-#     print(finder(files, queries))
+if __name__ == "__main__":
+    files = [
+        '/bin/foo',
+        '/bin/bar',
+        '/usr/bin/baz'
+    ]
+    queries = [
+        "foo",
+        "qux",
+        "baz"
+    ]
+    print(finder(files, queries))
 
 
 
