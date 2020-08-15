63c62d6673979ff4be50677c17f8c837650b9bee
stevekutz <stkutz@gmail.com>
2020-08-15 11:28:19 -0500

    completed ex_1, tests PASSED

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,13 +4,41 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
-    return None
+
+    index_dict = {}
+    result = []
 
 
+    # build hashtable with weights
+    for i in range(length):
+        index_dict[weights[i]] = i
+
+    # print(index_dict)
+
+    # iterate through to find max & min from complement
+    for i in range(length):
+        current = weights[i]
+        complement = limit - current
+
+        if complement in index_dict:
+            #print(complement)  
+            #result.append(complement)
+            # print(i)   
+            result.append(i)
+
+    if result:
+        result = sorted(result, reverse = True)
+        return result
+
+    return None   # if nothing found
+
+weights = [9]
+print(get_indices_of_item_weights(weights, 1, 9))  # None
 
 
-
+weights= [4, 4]
+print(get_indices_of_item_weights(weights, 2, 8))   # [1,0]
 
 
 weights = [ 4, 6, 10, 15, 16 ]  # length = 5, limit = 21
-print(get_indices_of_item_weights(weights, len(weights), 21))
+print(get_indices_of_item_weights(weights, len(weights), 21))   # [3,1]
