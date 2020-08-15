b8bf81bc675dea4fa11d5a0797de97d71022cb39
Michael Lowe <mrlowememphis@yahoo.com>
2020-08-15 11:26:09 -0500

    ex1.py complete test passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    
+    data = {}
+    # loop through weights
+    for i in range(len(weights)):
+        # grabb inicies
+        data[weights[i]] = i
+    for i in range(len(weights)):
+        dif = limit-weights[i]
+        if dif in data:
+            return (max(i, data[dif]), min(i, data[dif]))
 
     return None
