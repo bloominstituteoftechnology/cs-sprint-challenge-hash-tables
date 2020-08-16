5d0176c53ce00335b65eed6546baee81aad5a65a
Logan Negley <logannegley@gmail.com>
2020-08-16 18:51:39 -0500

    psudocode of ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,8 +1,30 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
-    """
-    
-    # Your code here
+    Given a box with a max weight 
+    ----limit
+    A list of weight amounts for each dumbell
+    ---- weights
+    Number of different weights to box up
+    ----length
 
-    return None
+    Find 2 dumbells whos weight sum equals limit
+    If the sum of weights don't equal limit return None
+
+    Create a hashtable then search the table to find sum of 2 weights that == limit
+    Store the weight as keys and list index as value
+
+    """
+    # Your code here
+    table = {}
+
+    for weight in weights:
+        table[weight]
+
+    print(table)
+
+
+
+    # return None
+
+    get_indices_of_item_weights([4,6,10,15,16], 5, 21)
