c258bfbeb6c8705300e6203ea7b72cc39579593a
David Cruz <dcman1211@gmail.com>
2020-09-04 12:04:45 -0400

    complete ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,15 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+d = {}
+
+def get_indices_of_item_weights(weights, length, limit):
+    
+    for i in range(length):
+        d[weights[i]] = weights.index(weights[i])
+
+    for i in range(length):
+        if limit - weights[i] in d:
+            x = d[limit - weights[i]]
+
+            return (x, d[weights[i]])
+
+# getting 1 failure on test 2
