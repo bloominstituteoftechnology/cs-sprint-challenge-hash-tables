8e8f64c3dffe55dae1658a46e500710f6e515b88
Joe V <joeveverka89@gmail.com>
2020-09-04 12:09:04 -0400

    ex4 MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,25 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# Make a hashtable that takes a num
+# Create an object that takes every num inserted and makes a corresponding negative
+# Check for nums
+# Iterate through nums checking to see if corresponding negative exists
+# If so, append to result
 
-    return result
+
+def has_negatives(a):
+
+	negatives_by_number = {}
+
+	for num in a:
+		negatives_by_number[num] = -num # this is our negative number to check
+
+	result = [] # array for nums with pos and neg
+
+	for num in negatives_by_number.keys(): # 
+		if num > 0 and negatives_by_number[num] in negatives_by_number:
+			result.append(num)
+    
+
+	return result
 
 
 if __name__ == "__main__":
