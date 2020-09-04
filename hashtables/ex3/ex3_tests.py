a1792db9de4f79de0c331447d6833309a1d4971b
Angela Flowers <me@angela.flowers>
2020-09-04 19:24:32 -0400

    revised ex3 smaller test case

--- hashtables/ex3/ex3_tests.py
+++ hashtables/ex3/ex3_tests.py
@@ -8,7 +8,7 @@ class TestEx2(unittest.TestCase):
     def test_small(self):
         result = intersection([
             [1,2,3],
-            [1,4,5],
+            [1,2,5],
             [1,6,7]
         ])
         self.assertTrue(result == [1])
