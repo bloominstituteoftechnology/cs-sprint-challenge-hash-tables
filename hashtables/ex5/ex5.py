ee3e5ecc5294fd7a20b76b286b90d53d58c5318c
jdblackstar <josh@blackstar.dev>
2020-09-04 10:18:33 -0700

    ex5 test passed

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -4,10 +4,48 @@
 
 def finder(files, queries):
     """
-    YOUR CODE HERE
+    goal: isolate filename from path, add to results if there is
+    a matching query
+
+
     """
     # Your code here
 
+    cache = {}
+    stash = [] # might need this
+
+    # loop through each path
+    for path in files:
+        # get the last 'word' from the path
+        filename = path.split("/")[-1]
+
+        # if filename in cache
+        if filename in cache:
+            # append that path to cache
+            cache[filename].append(path)
+        # else
+        else:
+            # do nothing? i guess
+            cache[filename] = [path]
+
+    # for each query
+    for query in queries:
+        # if the filename is in cache
+        if query in cache:
+            # append query to temp list
+            stash.append(cache[query])
+    
+    # instantiate results
+    result = []
+
+    # for each list in temp list
+    for j in stash:
+        # read through each list
+        for k in j:
+            # append each item to the results list
+            result.append(k)
+
+    # spit back results
     return result
 
 
