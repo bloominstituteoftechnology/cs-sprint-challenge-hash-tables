5565abc4787ff78053f18a9b6fa81ec9bf69dc2f
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-09-04 09:19:21 -0700

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,21 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # set up variables
+    # result list
+    result = []
+    # set up hashtable dict
+    my_dict = {}
+
+    # loop through the list
+    for num in a:
+        # set up num int in dict
+        my_dict[num] = num
+
+        # if number isn't 0 and in dict
+        if num != 0 and - num in my_dict:
+            # return number as pos and append
+            result.append(abs(num))
 
     return result
 
