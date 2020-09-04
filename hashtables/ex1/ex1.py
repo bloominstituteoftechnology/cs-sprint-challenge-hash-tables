6b1509b6801e907a3430160b97fadcbeb3833b93
Jasim Rashid <jasim.rashid@gmail.com>
2020-09-04 23:14:17 +0530

    stretch complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,16 +5,10 @@ def get_indices_of_item_weights(weights, length, limit):
 
     weights_dict = {}
     dict_to_index = {}
-    pos_1 = None
-    post_2 = None
     for i,v in enumerate(weights):
         if i not in weights_dict:
             weights_dict[v] = limit - v
             dict_to_index[limit-v] = i
-
-    
-    # print('weights:       ',weights_dict)
-    # print('dict to index:', dict_to_index)
 
     for i,v in enumerate(weights):
         if v in dict_to_index:
@@ -22,7 +16,7 @@ def get_indices_of_item_weights(weights, length, limit):
             return output
 
 
-
+# Testing:
 print(get_indices_of_item_weights([4,6,10,15,16],5,21))
 
 
