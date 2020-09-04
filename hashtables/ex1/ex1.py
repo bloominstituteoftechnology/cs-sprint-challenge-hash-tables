3bd2dfb0d0c71a6b0899767d1c41af3084bce603
Joe V <joeveverka89@gmail.com>
2020-09-04 11:15:49 -0400

    Initial Commit. Starting on ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,9 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+	weight_indices = {}
+
+	for i in range(length):
+		weight_indices[weights[i]] = i
+
 
     return None
