27a096c9738b57c4947a76b1aebc55c83daa2751
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-09-04 09:04:33 -0700

    ex2 Complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,22 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # set up dict
+    my_dict = {}
+    # setting up list (route)
+    route = [None] * length
+
+    # go through all tickets
+    for ticket in tickets:
+        # set dict source
+        my_dict[ticket.source] = ticket.destination
+    # reset my dict
+    next = my_dict["NONE"]
+
+    for r in range(0, length):  # iterate through range
+        # set route dict
+        route[r] = next
+        # set the next destination to dict
+        next = my_dict[next]
 
     return route
