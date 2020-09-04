d3a5f9aa1419dabfe55e729fcb23958312208d59
jdblackstar <josh@blackstar.dev>
2020-09-04 08:34:54 -0700

    attempt 1 on ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,25 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    desired output: tuple in form (zero, one)
+    linear time
+
+    example from README:
+    input: weights [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+    output: [ 3, 1 ] # since these are the indices of weights 15 and 6 whose sum equals 21
     """
     # Your code here
+
+    cache = {}
+
+    for i in range(length):
+
+        weight = weights[i]
+        pair = limit - weight
+
+        if pair in cache:
+            j = cache[limit]
+            return [i, j]
+        else:
+            cache[weight] = i
 
     return None
