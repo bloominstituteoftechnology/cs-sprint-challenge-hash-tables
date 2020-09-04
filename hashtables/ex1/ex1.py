17c36330d58a75ca43653b58ef4fe2a897c47fd8
Jasim Rashid <jasim.rashid@gmail.com>
2020-09-04 23:08:52 +0530

    updates

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,28 +2,6 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-        
-    # i = 0
-    # j = 1
-    # gap = 1
-    # sums = {}
-    # while i < length-1:
-    #     while j < length:
-    #         key = str(i)+'-'+str(j) if i<=j else str(i)+'-'+str(j)
-    #         if key not in sums:
-    #             sums[key] = nums[i] + nums[j]  
-    #         if(sums[key] == target):
-    #             return ([i,j] if i<=j else [j,i])
-    #         i += 1
-    #         j += 1
-    #     gap += 1
-    #     j = gap
-    #     i = 0 if gap < len(nums) else len(nums)
-
-    
-    # for i in range(length):
-    #     if limit - weights[i] in weights[i+1:]:
-    #         return [i, weights[i+1:].index(limit-weights[i]) + i + 1]
 
     weights_dict = {}
     dict_to_index = {}
@@ -35,15 +13,13 @@ def get_indices_of_item_weights(weights, length, limit):
             dict_to_index[limit-v] = i
 
     
-    print('weights:       ',weights_dict)
-    print('dict to index:', dict_to_index)
+    # print('weights:       ',weights_dict)
+    # print('dict to index:', dict_to_index)
 
     for i,v in enumerate(weights):
         if v in dict_to_index:
             output = [max(i,dict_to_index[v]), min(i,dict_to_index[v])]
-            # breakpoint()
             return output
-            # return([max(i,dict_to_index[v]),min(i,dict_to_index[v])])
 
 
 
