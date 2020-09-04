878fe5535c4ed9519fd3a306289f1b85b6c9ac86
Joe V <joeveverka89@gmail.com>
2020-09-04 11:50:31 -0400

    Not sure about the errors in ex2...moving on.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,28 +16,20 @@ def reconstruct_trip(tickets, length):
     # iterate through our tickets and find our ticket with Source None
     for ticket in tickets:
     	ticket_source[ticket.source] = ticket 
-
     	# now that we can use the ticket source as the 'ticket' we can find the ticket with source None
-
-    	source_none_ticket = ticket_source["NONE"] # Gotcha
-
+    	current_ticket = ticket_source['NONE']
+    	# Gotcha
     	# now let's set our route up
-
-    	my_route = []
+    	route = []
 
     	while True: 
-    		route.append(source_none_ticket)
+    		route.append(current_ticket.destination)
 
     		#if we see NONE for destination, keep going
-    		if source_none_ticket.destination == "NONE":
+    		if current_ticket.destination == "NONE":
     			break
 
     		# set next ticket to the current tickets destination 
-    		source_none_ticket = ticket_source[source_none_ticket.destination]
+    		current_ticket = ticket_source[current_ticket.destination]
 
-    	return my_route
-
-
-
-
-    return route
+    	return route
