e0feddf1be88fd83793cd1d0db6af969227a4139
Joe V <joeveverka89@gmail.com>
2020-09-04 11:28:20 -0400

    Ex 1. MVP.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-
 	weight_indices = {}
 
 	for i in range(length):
 		weight_indices[weights[i]] = i
 
+	for i in range(length):
+		our_target = limit - weights[i]
+		print(f"The target is: {our_target} - limit: {limit} - weights[i]: {weights[i]}")
+		if our_target in weight_indices:
+			if weight_indices[our_target] > i:
+				return (weight_indices[our_target], i)
+			else: 
+				return (i, weight_indices[our_target])
 
-    return None
+	return None
+	
+
