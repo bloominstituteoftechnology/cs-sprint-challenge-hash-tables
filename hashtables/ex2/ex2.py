2356347840f6d8ce6ad898956fc643697c1407d2
jdblackstar <josh@blackstar.dev>
2020-09-04 08:22:02 -0700

    add ex2 before test

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,44 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Highlights from README:
+
+    source (string) - starting airport
+    destination (string) - destination airport
+
+    pseudocode example:
+
+    tickets = [
+        Ticket{ source: "PIT", destination: "ORD" },
+        Ticket{ source: "XNA", destination: "CID" },
+        Ticket{ source: "SFO", destination: "BHM" },
+        Ticket{ source: "FLG", destination: "XNA" },
+        Ticket{ source: "NONE", destination: "LAX" },
+        Ticket{ source: "LAX", destination: "SFO" },
+        Ticket{ source: "CID", destination: "SLC" },
+        Ticket{ source: "ORD", destination: "NONE" },
+        Ticket{ source: "SLC", destination: "PIT" },
+        Ticket{ source: "BHM", destination: "FLG" }
+    ]
+
+    target output:
+    an array of strings with the entire route of your trip
+    for example above:
+
+    ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+
     """
     # Your code here
+
+    cache = {}
+    route = [None] * length
+    for t in tickets:
+        cache[t.source] = t.destination
+
+    flightplan = cache['None']
+
+    for flight in range(length):
+        route[flight] = flightplan
+        flightplan = cache[flightplan]
 
     return route
