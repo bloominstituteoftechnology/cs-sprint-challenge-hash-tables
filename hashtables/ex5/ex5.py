36e9d23511c716ebb35a51fad1491de71903f5c8
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-09-04 09:40:39 -0700

    ex5 Complete

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,36 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # set up dict
+    my_dict = {}
+    # results list
+    result = []
+    # making a dict of paths
+    for path in files:
+        # the file is the last name after last / divider
+        file_item = path.split('/')[-1]
+        # if item is in my_dict
+        if file_item in my_dict:
+            # append path to locations that are known
+            my_dict[file_item].append(path)
+        else:
+            # create a list if item is not in my_dict
+            my_dict[file_item] = [path]
+
+    #  loop to check queries
+    for q in queries:
+        # if query is in my_dict
+        if q in my_dict:
+            # set results to my_dict
+            results = my_dict[q]
+            # return a list of paths
+            # return lists into one list
+            for path in results:
+                # append list of path
+                result.append(path)
+    else:
+        # if not then pass
+        pass
 
     return result
 
