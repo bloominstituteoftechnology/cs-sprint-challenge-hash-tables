9da2394a8ff300b7cf6c17858c5428d211fe900a
connor sanderford <connorsanderford@gmail.com>
2020-09-11 20:01:55 -0700

    fifth problem done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -4,9 +4,23 @@
 
 def finder(files, queries):
     """
-    YOUR CODE HERE
+    given a list of filepaths, and a list of query filenames, returns those filepaths
+    whose filenames are in the query.
     """
-    # Your code here
+    
+    # init dict
+    d = {}
+
+    # add all the queries to the dict
+    for query in queries:
+        d[query] = query
+
+    # loop over the files, and check if the filename is in queries
+    result = []
+
+    for filename in files:
+        if filename.split('/')[-1] in d:
+            result.append(filename)
 
     return result
 
