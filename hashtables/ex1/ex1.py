6d92988f55b855824bca8b90a2e3bc587d9a7dcd
RyanSWest <west56309@gmail.com>
2020-09-12 12:50:33 -0500

    MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+
+    for key, value in enumerate(weights):
+        if value in cache:
+            cache[value].append(key)
+        else:
+            cache[value] = [key]
+
+    for value in cache:
+        if limit - value in cache:
+            if cache[limit - value] is cache[value]:
+                if len(cache[value]) >= 2:
+                    return (cache[limit - value][1], cache[value][0])
+            else:
+                return (cache[limit - value][0], cache[value][0])
 
     return None
+print(get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], length = 5, limit = 21))
