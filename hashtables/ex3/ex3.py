6f0cb09e14a43d40b35a55b7682cd630c3ea6e72
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-09-11 21:01:35 -0600

    ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,8 +2,31 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # will be passed in an array of arrays
+    # will cache the first array into the dictionary
+    
+    cache = {}
+    newCache = {}
+    for i in range(len(arrays)):
+        if i == 0:
+            for j in range(len(arrays[i])):
+                # doing the caching of the one dictionary
+                cache[arrays[i][j]] = ""
+        # for the rest will look in the dictionary if the item in the array is not in 
+        # the dictionary then the key will be removed from the dictionary
+        # by just adding that values that are in their to the newCache
+        # and then will rename the cache to what newCache holds
+        else: 
+            # doing the for loop for the rest of the arrays
+            for j in range(len(arrays[i])): 
+                if arrays[i][j] in cache:
+                    newCache[arrays[i][j]] = ""
+            # will now make cache = to newCache
+            cache = newCache
 
+    # now making result equal the cache
+    result = [*cache]
+    
     return result
 
 
