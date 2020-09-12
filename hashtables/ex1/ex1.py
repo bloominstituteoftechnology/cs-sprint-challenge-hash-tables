cff4e866c9b7f500a536d28c37beba791e0ba117
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-09-11 19:02:18 -0700

    finish ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,10 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_dict = {}
+    for index, weight in enumerate(weights):
+        if limit - weight in weight_dict:
+            return [index, weight_dict[limit-weight]]
+        weight_dict[weight] = index
 
     return None
