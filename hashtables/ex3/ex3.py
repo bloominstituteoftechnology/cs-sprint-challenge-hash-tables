75ea38faf76aa14ba5f95586b009d91d2f91414c
Dan Anderson <danfanderson@gmail.com>
2020-09-11 20:26:47 -0500

    Update ex3.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,10 +2,43 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Working objects/variables
+    arr_maps = []
+    arr_ret  = []
 
-    return result
+    # Iterate through the lists of lists
+    for lst in arrays:
+        # Translate the list to a dict with list values as keys
+        tmp_map = dict.fromkeys(lst)
 
+        # Append the map to our array (list) of maps
+        arr_maps.append(tmp_map)
+
+    # Done iterating, pop the first map from our array of maps
+    iter_map = arr_maps.pop(0)
+
+    # Iterate through the keys of the "popped" map
+    for itr_key in iter_map.keys():
+        flg_found = True
+
+        # Is this key in every other map?
+        #   Iterate through all the other maps and search
+        for tmp_map in arr_maps:
+            # Is the key being processed in this map?
+            if itr_key not in tmp_map:
+                # No, break out and continue with the next key
+                flg_found = False
+                break
+
+        # Was this key found in every map?
+        if flg_found:
+            # Yes. Add to our return object
+            arr_ret.append(itr_key)
+
+
+    # Done iterating, sort the return list and return the value
+    arr_ret.sort()
+    return arr_ret
 
 if __name__ == "__main__":
     arrays = []
