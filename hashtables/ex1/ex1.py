d88787ebc5add8dcf7677eaf20e7479fc480a821
Buddy Agyin <agyin3@gmail.com>
2020-09-11 20:24:52 -0600

    complete mvp challenge

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,14 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_hash = {}
 
+    for i , w in enumerate(weights):
+        smaller_idx = limit - w
+        if smaller_idx in weight_hash:
+            smaller = weight_hash[smaller_idx]
+            return (i, smaller)
+        
+        weight_hash[w] = i
+        
     return None
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -27,10 +27,6 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
-
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
