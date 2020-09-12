506ddd5e5f2365a599f48f497834cdb37f16ce26
Robert Keffury <bobby.keffury@me.com>
2020-09-11 21:33:06 -0700

    1 Completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    if length <= 1:
+        return None
+
+    package = {}
+
+    # Go through items in weights & add to the dictionary.
+    for item in range(length):
+        weight = weights[item]
+
+        if weight in package:
+            value = package[weight]
+            return (item, value)
+        
+        differece = limit - weight
+        package[differece] = item
 
     return None
