9145091cb73bae8dc793c581396ba4ccc7ac09bf
connor sanderford <connorsanderford@gmail.com>
2020-09-11 20:01:43 -0700

    fourth problem done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,26 @@
 def has_negatives(a):
     """
-    YOUR CODE HERE
+    given a list of numbers containing negative numbers,
+    returns those positive numbers with a corresponding negative element.
     """
-    # Your code here
+    
+    # init dict
+    d = {}
+
+    # loop over the list once - for each negative value
+    # add it's absolute value to the dict
+
+    for element in a:
+        if element < 0:
+            d[abs(element)] = element
+
+    # loop over a second time - when we find the value
+    # in the keys, append to results
+     
+    result = []
+    for element in a:
+        if element in d:
+            result.append(element)
 
     return result
 
