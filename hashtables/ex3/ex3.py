773f805d39d60666c1b40de08ddb5daf3812a791
Buddy Agyin <agyin3@gmail.com>
2020-09-11 19:59:58 -0600

    complete challenge with nested for loop

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,13 +2,34 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    numbers = {}
 
-    return result
+    for arr in range(len(arrays)):
+        results = []
+
+        for num in arrays[arr]:
+            if arr == 0:
+                numbers[num] = 1
+
+            elif arr == (len(arrays) - 1):
+                if num in numbers:
+                    results.append(num)
+            
+            else:
+                if num in numbers:
+                    numbers[num] += 1
+    
+
+
+    return results
 
 
 if __name__ == "__main__":
     arrays = []
+
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
