7d853f422824231cee5a84c788dc577a77304ced
jpeone <jesse.gene.peone@gmail.com>
2020-09-11 19:44:58 -0700

    first pass at the one-way flight.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,23 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    This is O(2n) (one to fill the table, one to look up the table) this rounds
+    out to O(n), linear time.
+    The space complexity is O(2n) as well, one for the table, one for the
+    return list.
     """
     # Your code here
+    # convert to a table
+    table = {}
+    for t in tickets:
+        table[t.source] = t.destination
+
+    # using range(length) will automatically chop off the last tick with a
+    # none destination. What even is a ticket with a none destination?
+    route = [] * length
+    key = 'NONE'
+    for i in range(length):
+        route[i] = table[key]
+        key = table[route[i]] # probs could have done table[key] as well
 
     return route
