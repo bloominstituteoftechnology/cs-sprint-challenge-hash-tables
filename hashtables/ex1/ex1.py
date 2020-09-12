435632621d579429d11457d459ad01673d9f5c58
jpeone <jesse.gene.peone@gmail.com>
2020-09-11 18:59:43 -0700

    first pass, not tested yet

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,27 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    This will have an O(weights + 1/2limit) worst case time complexity
+    This will have an O(weights) worst case space complexity
     """
     # Your code here
+    table = {}
+
+    for i, weight in enumerate(weights):
+        if weight in table:
+            table[weight].append(i)
+        else:
+            table[weight] = [i] # list to deal same weight objects
+
+    # limit // 2 - 1 because the minimum value for a lower weight has to be
+    # half of the weight limit. - 1 to compensate for range off by one default
+    # behavior
+    for higher in range(limit, (limit // 2) - 1, -1):
+        if higher in table:
+            lower = limit - higher
+            if lower in table:
+                if higher != lower:
+                    return (table[lower][0], table[higher][0])
+                elif len(table[higher]) > 1:
+                    return (table[higher][0], table[higher][1]))
 
     return None
