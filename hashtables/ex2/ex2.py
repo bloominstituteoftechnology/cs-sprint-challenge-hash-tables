de714d329eb8745fc0b2ae9b62fd9400675a88c9
codecookride <57050292+codecookride@users.noreply.github.com>
2020-09-11 20:46:04 -0700

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,26 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Create cache
+    set up list to length
+    for each ticket set starting location as key and destination as va;ue
+    iterate over the length of the dictionary and set by the next destination.
     """
-    # Your code here
+    cache = {} 
+
+    # setting up list (route)
+    route = [None] * length #len of length
+
+    #going thru the ticket in all tickets
+    for ticket in tickets: 
+        # setting source in my dictionary 
+        cache[ticket.source] = ticket.destination #to the destination
+    next = cache["NONE"] #resetting my dictionary 
+
+    for r in range(0, length): # iterating thru the range (over the length)
+        # set route flight to dictionary
+        route[r] = next 
+        # set the next destination to the dictionary
+        next = cache[next]
 
     return route
