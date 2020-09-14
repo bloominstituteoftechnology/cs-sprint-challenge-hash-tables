b49b90d0e6f2251e95ac268882a8c09e7a6177cb
Rob LeCheminant <roblecheminant@gmail.com>
2020-09-13 21:34:03 -0600

    attempted 5, couldn't figure out duplicate issue

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -20,38 +20,14 @@ def finder(files, queries):
 
 
 if __name__ == "__main__":
-    # files = [
-    #     '/bin/foo',
-    #     '/bin/bar',
-    #     '/usr/bin/baz'
-    # ]
-    # queries = [
-    #     "foo",
-    #     "qux",
-    #     "baz"
-    # ]
-    # print(finder(files, queries))
-
-
-    files = []
-
-    for i in range(500000):
-        files.append(f"/dir{i}/file{i}")
-
-    for i in range(500000):
-        files.append(f"/dir{i}/dirb{i}/file{i}")
-
-    queries = []
-
-    for i in range(1000000):
-        queries.append(f"nofile{i}")
-
-    queries += [
-        "file3490",
-        "file256",
-        "file999999",
-        "file8192"
+    files = [
+        '/bin/foo',
+        '/bin/bar',
+        '/usr/bin/baz'
     ]
-
-    result = finder(files, queries)
-    print(result)
+    queries = [
+        "foo",
+        "qux",
+        "baz"
+    ]
+    print(finder(files, queries))
