a94c54b949ec652dd4416890a695466a3a365cde
Mason Nystrom <masonnystrom@gmail.com>
2020-09-13 22:39:06 -0400

    Update ex2.py
    add ex 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,28 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Input: List of tickest where each ticket is a dict with source airport and destination airport
+    
+    Output: ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
     """
-    # Your code here
 
-    return route
+    # initial cache/hash table
+    cache = {}
+    # loop through tickets
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    # final destination = NONE
+    route = [cache["NONE"]]
+    
+    # get tickets in list of tickets to find next locations
+    for ticket in tickets:
+        
+        next_destination = route[-1]
+        
+        # append next_destination from cache
+        route.append(cache[next_destination])
+    
+        # when final route is None, append and return trip
+        if route[-1] == "NONE":
+            return route
+
