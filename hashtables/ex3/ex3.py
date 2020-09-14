1dc92c321e91cff61fc4d4d5b5f03b21513c742c
Eric <eric@erics-mbp.localdomain>
2020-09-13 20:49:29 -0500

    Completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,21 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    cache = {}
+
+    for index, arr in enumerate(arrays):
+        for num in arr:
+            if cache.get(num) is not None and index > 0:
+                cache[num] += 1
+            elif cache.get(num) is None and index == 0:
+                cache[num] = 1
+            else:
+                continue
+
+    for value in cache:
+        if cache[value] == len(arrays):
+            result.append(value)
 
     return result
 
