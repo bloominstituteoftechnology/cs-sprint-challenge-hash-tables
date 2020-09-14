ceea7c0533e3bb7964522e056de95a18f30bbda4
Eric <eric@erics-mbp.localdomain>
2020-09-13 20:31:05 -0500

    Completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    flights = {}
+    route = [None] * length
+
+    for i in range(0, length):
+        curr = tickets[i]
+        flights[curr.source] = curr.destination
+
+    route[0] = flights["NONE"]
+    route[1] = flights[route[0]]
+
+    if length > 2:
+        for i in range(2, length):
+            route[i] = flights[route[i-1]]
 
     return route
