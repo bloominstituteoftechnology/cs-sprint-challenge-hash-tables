0cede6a866e54427ad9a94b0db41570e7815180d
Rob LeCheminant <roblecheminant@gmail.com>
2020-09-13 21:31:53 -0600

    attempted 5, finished 2

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,57 @@
-# Your code here
-
+import ntpath
+import itertools
 
 
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    names = {}
+    file_temp = files
+    file_temp[:] = map(lambda x: (x, ntpath.basename(x)), files)
+    names.update([file for file in file_temp])
+
+    result = []
+    for item in queries:
+        if item in names.values():
+            result.append(list(names.keys())[list(names.values()).index(item)])
 
     return result
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
+    # files = [
+    #     '/bin/foo',
+    #     '/bin/bar',
+    #     '/usr/bin/baz'
+    # ]
+    # queries = [
+    #     "foo",
+    #     "qux",
+    #     "baz"
+    # ]
+    # print(finder(files, queries))
+
+
+    files = []
+
+    for i in range(500000):
+        files.append(f"/dir{i}/file{i}")
+
+    for i in range(500000):
+        files.append(f"/dir{i}/dirb{i}/file{i}")
+
+    queries = []
+
+    for i in range(1000000):
+        queries.append(f"nofile{i}")
+
+    queries += [
+        "file3490",
+        "file256",
+        "file999999",
+        "file8192"
     ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+
+    result = finder(files, queries)
+    print(result)
