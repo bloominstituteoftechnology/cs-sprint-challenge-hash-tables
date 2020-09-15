60023b61e98a134c66fbf625c2ab7380bcf197c7
Serina Grill <gril0044@umn.edu>
2020-09-14 22:55:04 -0400

    cleaned code

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -9,36 +9,18 @@ def has_negatives(a):
         # add all items to a dictionary
         d[index] = value
 
-    # empty list
-    positive_numbers = []
-
     pos_has_neg = []
 
-    # # for key in dict
+    # for key in dict
     for key, value in d.items():
 
-        if value < 1:
-
-            print('less than 1', value)
+        if value < 0:
 
             if -value in d:
-                if -value != 0:
-                    pos_has_neg.append(-value)
+            
+                pos_has_neg.append(-value)
 
     return pos_has_neg
-
-    # for key, value in d.items():
-        
-    #     # for num in positive_numbers:
-
-    #     #     if -num == value:
-
-    #     if -value in d:
-
-    #             pos_has_neg.append(-value)
-
-    # return pos_has_neg    
-
 
 
 if __name__ == "__main__":
@@ -47,7 +29,7 @@ if __name__ == "__main__":
 
     print(has_negatives([1,2,3]))
 
-    a = list(range(5000))
-    a += [-1,-2,-3]
+    # a = list(range(5000))
+    # a += [-1,-2,-3]
 
-    print(has_negatives(a))
+    # print(has_negatives(a))
