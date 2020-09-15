ff1b943596c86caf9139bc342301fdc6acbb4967
Jason Belgard <belgard.jason@gmail.com>
2020-09-14 20:11:44 -0500

    EX 5 MVP

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -8,6 +8,23 @@ def finder(files, queries):
     """
     # Your code here
 
+    paths = {}
+
+    for path in files:
+        good_file_name = path.split('/')[-1]
+
+        if good_file_name not in paths:
+            paths[good_file_name] = [path]
+        else:
+            paths[good_file_name].append(path)
+
+    result = []
+
+    for query in queries:
+        if query in paths:
+            for path in paths[query]:
+                result.append(path)
+
     return result
 
 
