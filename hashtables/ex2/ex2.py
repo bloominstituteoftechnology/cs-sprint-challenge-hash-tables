2993c20a9052ddf02eb7c851d1bca906938dd516
Serina Grill <gril0044@umn.edu>
2020-09-15 12:44:17 -0400

    completing last 2 exercises

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,24 +6,38 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    # all tickets
+    cache = {}
 
-    return route
+    # add all tickets to cache
+    for t in tickets:
+        cache[t.source] = t.destination
+
+    # list that returns destination
+    itinerary = []
+
+    # which stop we're currently on
+    current = 'NONE'
+
+    for i in range(length):
+
+        # append the value to the trip list
+        itinerary.append(cache[current])
+
+        # set a new current
+        current = cache[current]
+
+    return itinerary
 
 # We can hash each ticket such that the starting location is 
 # the key and the destination is the value. Then, when constructing 
 # the entire route, the ith location in the route can be found 
 # by checking the hash table for the i-1th location.
 
-# create hashtable
-# for i in range(3):
-    # 
-    
-    # if not in dict:
-        # d[source] = destination
+
+
+
 
 d = {
     'None': 'PDX',
@@ -39,3 +53,4 @@ tickets = [ticket_1, ticket_2, ticket_3]
 
 expected = ["PDX", "DCA", "NONE"]
 result = reconstruct_trip(tickets, 3)
+print(result)
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,18 +1,30 @@
-# Your code here
+def finder(paths, queries):
+    
+    q = {}
 
+    # add all queries to dict
+    for query in queries:
 
+        q[query] = query
 
-def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    matches = []
 
-    return result
+    for path in paths:
+
+        # for each part of the path (split of the delimiter '/')
+        for f in path.split("/")[1:]:
+
+            # if matches a query
+            if f in q:
+
+                # append matches
+                matches.append(path)
+
+    return matches
 
 
 if __name__ == "__main__":
-    files = [
+    paths = [
         '/bin/foo',
         '/bin/bar',
         '/usr/bin/baz'
@@ -22,4 +34,14 @@ if __name__ == "__main__":
         "qux",
         "baz"
     ]
-    print(finder(files, queries))
+    print(finder(paths, queries))
+
+        # Given a list of full paths to files, and a list of filenames to query,
+    # report all the full paths that match that filename.
+
+    
+        
+    # you would want to check each path item to see if it matches any of our queries, which we 
+    # have in our dict, if they do we could add the full path to a list (so don't mess up the path permanently)
+
+
