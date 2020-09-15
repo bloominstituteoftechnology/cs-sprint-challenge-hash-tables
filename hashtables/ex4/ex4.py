4a2e25399a6a1947a0fae3e595cba1f77bacf572
Jason Belgard <belgard.jason@gmail.com>
2020-09-14 19:50:40 -0500

    EX 4 MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,17 @@ def has_negatives(a):
     """
     # Your code here
 
+    negatives_by_number = {}
+
+    for num in a:
+        negatives_by_number[num] = -num #this defines our neg num to check
+
+    result = [] #array for nums with pos and neg
+
+    for num in negatives_by_number.keys():
+        if num > 0 and negatives_by_number[num] in negatives_by_number:
+            result.append(num)
+
     return result
 
 
