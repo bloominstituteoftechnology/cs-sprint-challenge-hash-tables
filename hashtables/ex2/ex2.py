418b29e68cb9be463e7247e64f1c5d8cbc99ab88
wildcard329 <enkiduwildman@gmail.com>
2020-10-02 09:12:40 -0700

    finished second challenge

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,26 @@ class Ticket:
         self.destination = destination
 
 
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    route = []
+    table = {}
+    for i in range(length):
+        table[tickets[i].source] = tickets[i].destination
+    start = "NONE"
+
+    def next_trip(destination):
+        next_flight = table.get(destination)
+        route.append(next_flight)
+        return next_flight
+
+    nt = next_trip(start)
+    while nt != "NONE":
+        nt = next_trip(nt)
 
     return route
+
