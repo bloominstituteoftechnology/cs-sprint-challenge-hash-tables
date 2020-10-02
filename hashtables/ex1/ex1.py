1a8351793d22ad81cd0c13a8cafc2f12416a073d
Cody Morley <codymorleyinc@gmail.com>
2020-10-02 09:19:31 -0700

    #1
    Woohoo!!

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,17 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    if length <=1:
+        return None
+    elif limit == (weights[0] + weights[1]):
+        return (1, 0)
+    else:
+        weights_lookup = {}
+
+        for x in range(length):
+            for y in range(1, length):
+                xy_sum = weights[x] + weights[y]
+                weights_lookup[xy_sum] = (x, y)
+
+        if limit in weights_lookup:
+            return weights_lookup[limit]
 
     return None
