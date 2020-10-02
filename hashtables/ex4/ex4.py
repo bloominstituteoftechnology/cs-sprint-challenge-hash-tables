fbfdef45d4b852ded361d783e08a230788a55655
MickeyLeeWells2020 <mlwells1051@gmail.com>
2020-10-02 10:35:24 -0600

    ex 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,9 +2,16 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = [] # result list
+    my_dict = {} #setting up hash table dict
 
-    return result
+    # for loop to loop thru the list 
+    for pos_num in a: 
+        my_dict[pos_num] = pos_num # setting up pos_num int in my dictionary
+
+        if pos_num != 0 and - pos_num in my_dict: #if number not 0 or in dictionary 
+            result.append(abs(pos_num)) #return number as positive number by appending
+# return result
 
 
 if __name__ == "__main__":
