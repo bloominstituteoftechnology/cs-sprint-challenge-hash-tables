8dd471840f6057816f70b08afca436a67e2d34f3
Patrick Millet <lohs79seahawk@gmail.com>
2020-10-02 10:42:40 -0500

    exercise 1 working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    weight_index = {}
+
+    for n in range(length):
+        weight_index[weights[n]] = n
+
+    for n in range(length):
+        tar = limit - weights[n]
+        print(f"target: {tar} = limit: {limit} - weights[n]: {weights[n]}")
+        if tar in weight_index:
+            # Need to order by larger index first1
+            if weight_index[tar] > n:
+                return (weight_index[tar], n)
+            else:
+                return (n, weight_index[tar])
+
     return None
