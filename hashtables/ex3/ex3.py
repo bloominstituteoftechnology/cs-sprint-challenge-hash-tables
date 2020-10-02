d95d52bc0721e3eeec367b008273a9b8969a14c1
Anthony N <foto.arn@gmail.com>
2020-10-02 09:22:05 -0700

    + ex3.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,22 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    
+    num_count = {}
+
+    # Init arrays
+    for i in arrays:
+        for k in i:
+            if k in num_count:
+                num_count[k] += 1
+            else:
+                num_count[k] = 1
+    # Checking to see 
+    # Check if appears in all arrays
+    for num in num_count:
+        if num_count[num] == len(arrays):
+            result.append(num)
+        else:
+            continue
 
     return result
 
@@ -15,5 +29,3 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
-
-## testing testing 1 2 3 
