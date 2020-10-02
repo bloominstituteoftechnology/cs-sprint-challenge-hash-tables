dae5d5f5496f0a10e7a7ba404ac10e8010a63c48
MaryamMosstoufi <maryam@zooxoos.com>
2020-10-02 11:23:42 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,20 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    if length < 2:
+        return None
+    for i in range(0, length):
+        for j in range(1, length):
+            sum = weights[i] + weights[j]
+            if sum == limit:
+                if weights[i] > weights[j] or weights[j] == 0:
+                    if weights[j] == 0:
+                        return (j, i)
+                    else:
+                        return (i, j)
+                else:
+                    if weights[i] == 0:
+                        return (i, j)
+                    else:
+                        return (j, i)
     return None
