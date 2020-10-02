2f6402b2f1306ad52a6bec62c346fc513ab34b54
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-10-02 13:08:28 -0400

    mvp met

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,8 +2,24 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # make a cache
 
+    cache = {}
+    # we need to loop through every array inside our list of arrays
+    for miniarray in arrays:
+        # for every number inside the nested array
+        for item in miniarray:
+            if item not in cache:
+                cache[item] = 0
+            else:
+                cache[item] += 1
+
+    result = []
+    # if that index's value is equal to the amount of arrays, it was in all of them.
+    # add it to results and return results
+    for key in cache:
+        if cache[key] == len(arrays) - 1:
+            result.append(key)
     return result
 
 
