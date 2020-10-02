26c69357ecb96b5e15b86e87576c32a0d99dda43
wildcard329 <enkiduwildman@gmail.com>
2020-10-02 10:24:45 -0700

    completed all challenges

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,7 +7,30 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    copy = files
+    table = {}
+    end_path = []
+    result = []
 
+    for f in copy:
+        string = ''
+        while f[-1] != '/':
+            string += f[-1]
+            f = f[:-1]
+        string = string[::-1]
+        end_path.append(string)
+
+    for i in range(len(files)):
+        if end_path[i] not in table:
+            table[end_path[i]] = []
+        table[end_path[i]].append(files[i])
+
+    for q in queries:
+        if q in table:
+            r = table.get(q)
+            for a in r:
+                result.append(a)
+    
     return result
 
 
