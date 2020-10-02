365e773aaeff8426385ec299d18791b1bb64146f
wildcard329 <enkiduwildman@gmail.com>
2020-10-02 09:43:00 -0700

    completed fourth challenge

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    table = set()
+    result = []
+    
+    for entry in a:
+        if entry < 0 and entry not in table:
+            table.add(entry)
+
+    for entry in a:       
+        if -(entry) in table:
+            result.append(entry)
 
     return result
 
