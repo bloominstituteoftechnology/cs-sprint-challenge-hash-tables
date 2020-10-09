e3c5aebc8b853ac2c64452cfa86dd11e9df923c1
dallasjames <dallasjames42@gmail.com>
2020-10-09 17:16:54 -0600

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,13 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return None
+    combo = {}
+    if len(weights) > 1:
+        for i in range(0, len(weights)):
+            w = weights[i]
+            if w in combo:
+                val = combo[w]
+                return [i, val]
+            diff = limit - w
+            combo[diff] = i
+        return []
+    else:
+        return None
