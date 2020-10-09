30e5d91725a9e2cff79f4a0a974bb1dda50b2fb6
Andrew Haberman <a.haberman25@gmail.com>
2020-10-09 19:43:31 -0400

    ex2 tests pass

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,15 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = [None] * length
 
+    cache = {}
+
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    next = cache["NONE"]
+
+    for curr in range(0, length):
+        route[curr] = next
+        next = cache[next]
     return route
