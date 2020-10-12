cd2914e6ce162cc11bd3003ed41cc8652d0acc55
Samantha-Reis <Samantha-Reis@users.noreply.github.com>
2020-10-12 00:45:29 -0400

    all done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,20 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+# limit - weight limit
+# weights - list of weights
 
-    return None
+    cache = {}
+
+# Your code here
+# iterate through our list of wights by index
+    for i in range(length):
+        cache[weights[i]] = i
+
+# iterate list of weights and find matching weights
+    for index, weight in enumerate(weights):
+        w = limit - weight
+
+        if w in cache:
+            i = cache[w]
+            return (index, i) if index > i else (i, index)
