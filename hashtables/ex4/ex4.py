381df32b02c2e4f5ad9369a0af905214db713fa6
Toniddarden1 <Toniddarden@gmail.com>
2020-10-12 22:04:06 -0400

    ex5 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,14 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-    # anom func
+    # anom func to sort through 
     a.sort(key=lambda x: x + 0.5 if x >= 0 else -x)
     # collection of pairs 
     pairs_ = []
-
-    # Return a zip object whose .__next__() method returns a tuple where the i-th element comes from the i-th iterable argument. 
+    
+#   enumerate pairs of consecutive list entries.
     for pair in [(x, y) for x, y in zip(a, a[1:]) if x == -y]:
-        # append each pair 
+        # append each pair int
         pairs_.extend(pair)
         
     # append pos nums only 
