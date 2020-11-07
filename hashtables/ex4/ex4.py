09f15b6c1ceea10cd54e99936ed4ad3e96b71ffa
rubyclaguna <rubyclaguna@gmail.com>
2020-11-06 20:16:45 -0800

    ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,22 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    neg = {} # initialize empty dict 
+
+    # loop through (a), add abs val to neg 
+    for i in a: 
+        if i < 0: 
+            neg[abs(i)] = i
+    
+    result = [] # intialize empty list for results 
+
+    # loop through (a), append to result if val in keys
+    for i in a: 
+        if i in neg: 
+            result.append(i)
+
 
     return result
+
 
 
 if __name__ == "__main__":
