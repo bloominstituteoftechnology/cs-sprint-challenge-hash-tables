71745915d12f70c9cfb5c3a60502abe982b8105f
Enayatullah <enayatullahna@gmail.com>
2020-11-06 18:58:59 -0800

    Completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,18 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    for i in range(len(weights)):
+        w = limit - weights[i]
+
+        if w in cache:
+            if cache[w] < i:
+                return [i, cache[w]]
+            else:
+                return [cache[w], i]
+
+        else:
+            cache[weights[i]] = i
 
     return None
