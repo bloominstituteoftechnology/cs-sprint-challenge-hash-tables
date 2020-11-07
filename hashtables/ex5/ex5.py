668db4bc336c008e2078a1ef67639fb3d863647c
tigju <tigju24@gmail.com>
2020-11-07 14:32:01 -0500

    Ex5 File Finder

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,21 +1,35 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    my_list = []
+    my_dict = {}
 
-    return result
+    for v in files:
+        f_name = v.split("/")[-1]
+        
+        if f_name in my_dict:
+            my_dict[f_name].append(v)
+        else:
+            my_dict[f_name] = [v]
+    
+    for q in queries:
+        if q in my_dict:
+            my_list.extend(my_dict[q])
+    
+    return my_list
 
 
 if __name__ == "__main__":
     files = [
         '/bin/foo',
         '/bin/bar',
-        '/usr/bin/baz'
+        '/usr/bin/baz',
+        'usr/foo/foo'
     ]
     queries = [
         "foo",
