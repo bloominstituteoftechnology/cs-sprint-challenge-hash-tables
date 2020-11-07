6554b75020444f797427dece6cab0ae2ead644d9
Khislatz <47505600+Khislatz@users.noreply.github.com>
2020-11-07 14:46:00 -0500

    ex 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    negatives_dict = {}
+    result = []
+
+    for num in a:
+        negatives_dict[num] = num
+
+        if num != 0 and -num in negatives_dict:
+            result.append(abs(num))
 
     return result
 
