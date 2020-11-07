103fcc42a7baf464418c7feae96c12828b551a30
ryanp <ryanpanimation@gmail.com>
2020-11-06 20:51:22 -0600

    ex4 test OK

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    table = dict()
+    result = []
+    a.sort()
+
+    for num in a:
+        if num < 0:
+            table[-num] = True
+        elif num > 0:
+            if table.get(num) is True:
+                result.append(num)
 
     return result
 
