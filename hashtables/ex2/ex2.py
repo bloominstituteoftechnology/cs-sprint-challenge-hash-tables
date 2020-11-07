57d0149028c72c46c700a19568cd048a904c3350
tigju <tigju24@gmail.com>
2020-11-07 13:06:16 -0500

    Ex 2 one-way-flight

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,39 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # create hashtable
+    hashtable = {}
+    # create list to hold the route
+    route = [None]*(length-1)
+
+    # go through the tickets and map source and destrination
+    for t in tickets:
+        # if source is note put it as the first element in array (as the first destination)
+        if t.source == "NONE":
+            route[0] = t.destination
+        # otherwise add to hashtable
+        else:
+            hashtable.update({t.source: t.destination})
+
+    # go through the length and check if current key is the next key's value and append it
+    for i in range(1, length-1):
+        route[i] = hashtable[route[i-1]]
 
     return route
+
+
+
+tickets = [
+    Ticket("PIT", "ORD"),
+    Ticket("XNA", "CID"),
+    Ticket("SFO", "BHM"),
+    Ticket("FLG", "XNA"),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO"),
+    Ticket("CID", "SLC"),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT"),
+    Ticket("BHM", "FLG")
+]
+
+print(reconstruct_trip(tickets, 10))
