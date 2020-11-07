35978b48c258dee9ed31b6dd9bf4e844cae0ef57
scrunts23 <scrunts23@gmail.com>
2020-11-07 09:38:49 -0600

    first one completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,14 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    index = {}
+
+    for i in range(len(weights)):
+        # check if hash table contains an entry for limit - weight
+        if limit - weights[i] in index:
+            # if true, return two items whose weight sum up to the limit
+            return [i, index[limit - weights[i]]]
+        else:
+            index[weights[i]] = i
 
     return None
