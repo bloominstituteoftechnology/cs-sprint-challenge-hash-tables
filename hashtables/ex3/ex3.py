6b5cb5ca447416aa656d0220dc323709474eecc7
tigju <tigju24@gmail.com>
2020-11-07 13:29:00 -0500

    Ex3 Array intersection

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,8 +2,25 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # define hashtable
+    my_dict = {}
 
+    # define array to store intersection numbers
+    result = []
+    # go through arrays of arrays
+    for arr in arrays:
+        # check every number in current array
+        for num in arr:
+            # if this number exists already in hashtable, then add it to array
+            if num in my_dict:
+                result.append(num)
+            # if not add to hashtable
+            else:
+                my_dict[num] = 1
+    # extract unique numbers from array converting into dict 
+    # and then take only keys by making it a list
+    result = list(dict.fromkeys(result))
+          
     return result
 
 
@@ -13,5 +30,8 @@ if __name__ == "__main__":
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # arrays.append(list(range(10, 20)) + [1, 2, 3])
+    # arrays.append(list(range(20, 30)) + [1, 2, 3])
+    # arrays.append(list(range(30, 40)) + [1, 2, 3])
 
     print(intersection(arrays))
