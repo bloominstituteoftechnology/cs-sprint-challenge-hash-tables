33103258d37511953d4a08b1080039bff397da60
Claudia Maciel <clc_80@yahoo.com>
2020-11-06 19:24:32 -0800

    ex3.py complete

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,21 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    intersection_dict = {}
+    result = []
+
+    # this is going to be used to make sure we have the right number of elements
+    num_of_arrays = len(arrays)
+    for array in arrays:
+        # add elements to dictionary
+        for i in array:
+            if i not in intersection_dict:
+                intersection_dict[i] = 1
+            else: 
+                intersection_dict[i] += 1
+
+    #iterate through the dictionary and get the items that exist the required times
+    for i in intersection_dict:
+        if intersection_dict[i] == num_of_arrays:
+            result.append(i)
 
     return result
 
