295f1c35a8d16f94dfadd3ed964d76a1abeb25d8
rubyclaguna <rubyclaguna@gmail.com>
2020-11-06 19:06:49 -0800

    ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    ht = {} # initialize empty dict 
+
+    for i, v in enumerate(weights):
+
+        # ht contains an entry for weight limit - weight value
+        if limit - v in ht:
+
+            # index of second value
+            pair = ht.get(limit-v), i
+            return tuple(sorted(pair, reverse=True))
+
+        # if not in hashtable, create entry 
+        else:
+
+            ht[v] = i
 
     return None
