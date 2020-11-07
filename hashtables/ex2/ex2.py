f52a7ee823bf2016a87203bd29eafe3d9e711ea2
Brian <bri2210433@yahoo.com>
2020-11-06 19:43:25 -0800

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,19 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = []
 
+    # loop though the tickets and set cache from source to destination
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+
+    # cache needs to equal some to go thorough while loop
+    destination = cache["NONE"]
+    while destination != "NONE":
+        #append the destion to the route and set that to the cache now
+        route.append(destination)
+        destination = cache[destination]
+    # have to add NONE to the list
+    route.append("NONE")
     return route
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for num in a:
+        # see if there is another instance of the number in our array positive or negative
+        if cache.get(abs(num)):
+            if (cache.get(abs(num)) + num) == 0:
+                result.append(abs(num))
+        else:
+            cache[abs(num)] = num 
 
     return result
 
