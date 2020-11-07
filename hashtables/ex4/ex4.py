1c7ed77c675f4e062cb002e586c6c3f0bf8e4bb5
scrunts23 <scrunts23@gmail.com>
2020-11-07 10:05:39 -0600

    all ex completed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,7 +1,19 @@
+from typing import List
+
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    cache = {}
+
+    for i in a:
+        cache[i] = -i
+
+    # return(cache)
+    negatives = {k:v for k, v in cache.items() if k < 0}
+
+    return(list(set(negatives.values()).intersection(set(a))))
 
  
 
