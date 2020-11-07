2b6c2e87cebbc6bbf70530a9ab28133d0803fe48
ryanp <ryanpanimation@gmail.com>
2020-11-06 20:34:27 -0600

    ex2 test OK

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,40 @@ class Ticket:
         self.destination = destination
 
 
+tickets = [
+    Ticket("PIT", "ORD"),
+    Ticket("XNA", "CID"),
+    Ticket("SFO", "BHM"),
+    Ticket("FLG", "XNA"),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO"),
+    Ticket("CID", "SLC"),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT"),
+    Ticket("BHM", "FLG"),
+]
+
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    table = dict()
+
+    for t in tickets:
+        table[t.source] = t.destination
+
+    route = []
+    start = table["NONE"]
+    route.append(start)
+
+    while len(route) < length:
+        for key in table:
+            if key == route[-1]:
+                route.append(table[key])
 
     return route
+
+
+print(reconstruct_trip(tickets, 10))
