8482e969cfe3f2910cbe38789248c75355a8e350
Veto Ramirez <vetorobert3@gmail.com>
2020-11-07 11:22:01 -0800

    Still working on it

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,6 +5,26 @@ class Ticket:
         self.destination = destination
 
 
+    def get_source(self):
+        return self.source
+    
+    # `destination` = next airport along my trip
+    def get_destination(self):
+        return self.destination
+
+#  Hash each ticket such that the starting location is the key and the destination is the value.
+def hash_tickets(tickets, length):
+    dict = {}
+    # looop through array of tickets
+    for i in range(length):
+        ticket = tickets[i]
+        # add starting airport(source) as key and destination as value
+        source, destination = ticket.get_source(), ticket.get_destination()
+        # destination equal to source
+        dict[source] = destination
+    return dict
+
+
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,20 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+        storage, result = set(), []
+    
+    # for each element of array
+    for n in numbers:
+        # find the negative value of numbers[n] 
+        diff = 0 - n
+        # if absolute values are equal to positive numbers
+        if diff in storage:
+            # store its absolute value in another vector
+            result.append(abs(diff))
+        # else for each term in vector, print first its negative value and the positive value
+        else:
+            # add the positive number with negative value in the array
+            storage.add(n)
 
     return result
 
