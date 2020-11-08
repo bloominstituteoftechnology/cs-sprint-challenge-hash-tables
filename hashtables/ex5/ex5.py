7106ac4a191b0df68028deaba5700ae0fe514486
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-11-08 18:31:21 -0500

    ex5 done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,24 @@
-# Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    for path in files:
+        file = path.split('/')[-1]
+
+        if file in cache:
+            cache[file].append(path)
+        else:
+            cache[file] = [path]
+
+    result = []
+    for a in queries:
+        if a in cache:
+            newResult = cache[a]
+            for path in newResult:
+                result.append(path)
 
     return result
 
