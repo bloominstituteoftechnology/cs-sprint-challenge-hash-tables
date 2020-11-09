360fd5ec40a9930d0d1ffc6844779444688eb773
Zach Hamilton <zlhamilton90@gmail.com>
2020-11-08 21:11:57 -0500

    first exercise complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -15,8 +15,3 @@ def get_indices_of_item_weights(weights, length, limit):
             else:
                 return (weight_dict[complement], i)
     return None
-
-
-weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
-answer_4 = get_indices_of_item_weights(weights_4, 9, 7)
-print(answer_4)
