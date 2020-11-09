161f6ba5fe3422cb9ecc09ade242368dee056a21
Neha-Kumari31 <Neha-kumari31@user.noreply.github.com>
2020-11-08 22:16:21 -0500

    working on the stretch

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,36 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    #create a dic to hold source and destination value
+    trip ={}
+    for ticket in tickets:
+        trip[ticket.source] = ticket.destination
+    #create a list to store next route
+    route =[] 
+    #start with source value none
+    next_route = trip['NONE']
+    #while next route is not distination(none)
+    while next_route != "NONE" :
+        #add to the distination in the route
+        route.append(next_route)
+        #change value of  the next_route 
+        next_route = trip[next_route]
+    route.append('NONE')
     return route
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets, 10))
+
