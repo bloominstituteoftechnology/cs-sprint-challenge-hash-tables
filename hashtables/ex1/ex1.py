7ed897c31f9f4a725c60f0ccc0745abec4e4c40f
Zach Hamilton <zlhamilton90@gmail.com>
2020-11-08 21:11:23 -0500

    first exercise complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,21 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_dict = {}
+    for i, weight in enumerate(weights):
+        weight_dict[weight] = i
 
+    for i in range(len(weights)):
+        value = weights[i]
+        complement = limit - value
+        if complement in weight_dict:
+            if i > weight_dict[complement]:
+                return (i, weight_dict[complement])
+            else:
+                return (weight_dict[complement], i)
     return None
+
+
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+answer_4 = get_indices_of_item_weights(weights_4, 9, 7)
+print(answer_4)
