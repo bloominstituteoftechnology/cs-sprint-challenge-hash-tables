1dd504916510fe5591e8fd0afed8ad5f569f0901
Jeff Breig <jjbreig@gmail.com>
2020-11-08 21:43:56 -0500

    finished second task

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,19 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    # Assume all ticket chains work!
+    ticket_cache = {}
+    route = [None] * length
+
+    for ticket in tickets:
+        ticket_cache[ticket.source] = ticket.destination
+
+    route[0] = ticket_cache["NONE"] #Establishing first ticket
+
+    for i in range(1, length):
+        route[i] = ticket_cache[route[i-1]]
+    
+    print(ticket_cache)
+
 
     return route
