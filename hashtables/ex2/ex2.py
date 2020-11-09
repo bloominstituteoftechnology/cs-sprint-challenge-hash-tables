18486bd3aa941854bc71180e4f1d8868c37f3019
Zach Hamilton <zlhamilton90@gmail.com>
2020-11-08 21:24:45 -0500

    completed exercise 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,14 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    route_dict = {}
+    for each in tickets:
+        route_dict[each.source] = each.destination
+    
+    route = []
+    next_stop = route_dict['NONE']
+    while next_stop != 'NONE':
+        route.append(next_stop)
+        next_stop = route_dict[next_stop]
+    route.append(next_stop) # 'NONE' still needs to be added to the route
     return route
