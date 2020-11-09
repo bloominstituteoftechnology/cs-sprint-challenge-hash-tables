ed2d249e7e6d9eff6e23146e13bb74653abd691a
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-11-08 19:48:52 -0800

    Finished first, working through second

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,22 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
+
+    if length <= 1:
+        return None
+
+    for i in range(length):
+        # start with weight
+        weight = weights[i]
+        dict[weight] = i
+    for n in range(length):
+        weight = weights[n]
+        difference = limit - weight
+        if difference in dict:
+            return (dict[difference], n)
+    # print(dict)
 
     return None
+
+print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,28 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
 
-    return route
+    for ticket in tickets:
+        dict[ticket.source] = ticket.destination
+
+    print(f'{dict}')
+
+    # return route
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "CID")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("CID", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+           ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets, 10))
