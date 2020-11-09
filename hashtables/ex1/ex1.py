3e843c592d8c2ce02a070d9c787039d1b7dbac2b
Diego Arriola <diego@macbook-pro.lan>
2020-11-08 21:26:47 -0500

    first problem solved

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,41 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    Understand:
+    weights = [12]
+    print(get_indices_of_item_weights(weights, 1, 12))  # None
+
+
+    weights = [4, 3, 1, 5]
+    print(get_indices_of_item_weights(weights, 2, 9))   # [3,0]
+
+
+    weights = [4, 6, 10, 15, 16]  # length = 5, limit = 21
+    print(get_indices_of_item_weights(weights, len(weights), 21))   # [3,1]
+
+    Plan:
+    Create a dictionary with the weights as values and index as key
+
+
     """
-    # Your code here
+    my_dict = {}
+    result = []
+    if len(weights) >= 1:
+        for i, w in enumerate(weights):
+            my_dict[w] = i
+        # print(my_dict)
+    for i in range(length):
+        compliment = limit - weights[i]
+        if compliment in my_dict:
+            result.append(i)
+    if result:
+        return sorted(result, reverse=True)
 
     return None
+
+
+if __name__ == '__main__':
+    weights = [4, 6, 10, 15, 16]  # length = 5, limit = 21
+    print(get_indices_of_item_weights(weights, len(weights), 21))
+
+    weights = [4, 4]
+    print(get_indices_of_item_weights(weights, 2, 8))   # [1,0]
