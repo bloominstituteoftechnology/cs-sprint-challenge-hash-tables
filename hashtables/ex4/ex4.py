d9851230e1a2b30186a81ad440b3234f524da551
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-11-09 23:37:41 +0900

    Complete ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,13 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    ht = {}
     result = []
-    for num_1 in a:
-        for num_2 in a:
-            if num_1 == num_2 * -1:
-                result.append(num_1)
-    return result
+    
+    for num in a:
+        ht[num] = True
 
+    for num in a:
+        if num * -1 in ht and num > 0:
+            result.append(num)
+    return result
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
