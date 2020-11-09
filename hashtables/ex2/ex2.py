6be12d0a8e355cfb2e9fddc06b2121a3b38210eb
Diego Arriola <diego@macbook-pro.lan>
2020-11-08 21:43:30 -0500

    second problem solved

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,30 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Understand:
+    To save time we have a test file lol
+
+    Plan:
+    create a dict with the source as the key and the destination as the values
+    find the initial airport which would be the source be "NONE"
     """
     # Your code here
+    dict_trip = {}
+    route = []
+
+    if len(tickets) == 0:
+        print("No tickets, no route")
+        return None
+
+    for ticket in tickets:
+        dict_trip[ticket.source] = ticket.destination
+
+    airport = dict_trip["NONE"]
+
+    i = 0
+    while i < length:
+        route.append(airport)
+        i += 1
+        airport = dict_trip[airport]
 
     return route
