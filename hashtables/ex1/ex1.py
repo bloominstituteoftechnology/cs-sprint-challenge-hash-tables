614f7adfa8126c1c0ac88e972f3bd28f30ab0118
Matt Martindale <martindalematt18@yahoo.com>
2020-11-08 19:15:10 -0700

    Finished ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,3 +1,15 @@
 def get_indices_of_item_weights(weights, length, limit):
+    result_dict = {}
+
+    if len(weights) <= 1:
+        return None
+
+    for index, key in enumerate(weights):
+        result_dict[key] = index
+
+    for index, key in enumerate(weights):
+        difference = limit - key
+        if difference in result_dict:
+            return (sorted([index, result_dict[difference]], reverse=True))
 
     return None
