c43a8b1a18551616d741600f8806abca704e6ad3
Jeff Breig <jjbreig@gmail.com>
2020-11-08 22:28:06 -0500

    test 4 passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,15 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
 
+    for num in a:
+        if abs(num) in cache:
+            result.append(abs(num))
+        else: 
+            cache[abs(num)] = num
+            
     return result
 
 
