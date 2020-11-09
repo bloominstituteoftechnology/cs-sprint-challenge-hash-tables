12cbe46fdbe1ea489d05b859e3912a7a6201d554
rfamilara <techknohauz.com>
2020-11-08 22:02:21 -0600

    Completed code for hashtables ex3 -Tests were successful

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,24 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    d = {}
+
+    for index, l in enumerate(arrays):
+        for num in l:
+            if num not in d:
+                # set a key for that number with it's value as an array containing the index of list it appeared in 
+                d[num] = [index]
+            else:
+                # add the current list index to the entry for this num 
+                d[num].append(index)
+
+    result = []
+
+    for key in d:
+        # might need to change to tuple here 
+    
+        if len(d[key]) == len(arrays):
+            result.append(key)
 
     return result
 
