6ff322e8e7cea468d12a9f512b3f4caad0a4335c
kmk028 <kmk028@users.noreply.github.com>
2020-11-08 19:07:20 -0800

    ex1 done with UPER steps

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,7 +5,11 @@ def get_indices_of_item_weights(weights, length, limit):
     output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
 
     Plan:
-    create a dict with weights as keys and corresponding values indices as values with enumerate
+   1. create a dict with weights as keys and corresponding indices as values with enumerate. 
+    2.now enumerate the weights list 
+    3.if (limit-element in weight list) present in dict , compare the value at that key with key for dict 
+    creted in step2.
+    4.Return the tuple with decending order.
 
     """
     ht = {}
