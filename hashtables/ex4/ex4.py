042f4aa0540a32420ba23b2667c017e75d3734fd
Oliver Abreu <oliverabreu@OliverAbreu-PC.attlocal.net>
2020-11-14 14:52:41 -0500

    feat(ex4.py): Provides test-passing result

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,21 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    # WE'VE GOTTA HAVE A CACHE, RIGHT?
+    cache = {}
+    # WE'VE GOTTA HAVE A RESULT ARRAY, EH?
+    result = []
+    # WHY DON'T WE ADD THE ABSOLUTE VALUES OF ALL...
+    # ...THE NEGATIVE NUMBERS INTO THE CACHE?
+    for value in a:
+        if value < 0:
+            cache[abs(value)] = True
+    # WE CAN NOW COMPARE THE VALUES IN THE INPUT...
+    # ...ARRAY AGAINST THE VALUES IN THE CACHE, NO?
+    for value in a:
+        if value in cache:
+            # IF A VALUE APPEARS IN BOTH THE INPUT...
+            # ...ARRAY AND THE CACHE, WHY NOT APPEND...
+            # ...THAT TO THE RESULTS ARRAY?
+            result.append(value)
     return result
 
 
