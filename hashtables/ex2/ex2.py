aa5598ff5f50461d8df45d792c1924cf2073e24a
Steven Waldron <stevenwaldron@stevens-mbp.attlocal.net>
2020-11-13 21:33:20 -0800

    MVP

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,6 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+from collections import deque
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +8,38 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dict = {}
+    for ticket in tickets:
+        t = {ticket.source:ticket.destination}
+        dict.update(t)
+        
+    
+    route = []
+    firstdestination = dict.pop("NONE")
+    route.append(firstdestination)
+    curr = route[0]
+    for ticket in dict:
+        nextdestination = dict.get(curr)
+        route.append(nextdestination)
+        curr = route[route.index(curr)+1]
+    # route.remove('NONE')
+    
 
     return route
+
+tickets = [
+    Ticket("PIT", "ORD" ),
+    Ticket("XNA", "CID" ),
+    Ticket("SFO", "BHM" ),
+    Ticket("FLG", "XNA" ),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO" ),
+    Ticket("CID", "SLC" ),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT" ),
+    Ticket("BHM", "FLG" )
+]
+
+expected = ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+
+print(reconstruct_trip(tickets,len(tickets)))
